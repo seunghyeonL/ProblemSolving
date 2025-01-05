d1052493ac6f4b3d433f86bae5e634c85de87c9f
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-01-05 13:43:57 +0900

    [level 1] Title: 옹알이 (2), Time: 0.04 ms, Memory: 4.44 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
@@ -1,56 +1,32 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<string> babbling) {
     int answer = 0;
-    unordered_map<char, string> speaks;
-
-    speaks['a'] = "aya";
-    speaks['y'] = "ye";
-    speaks['m'] = "ma";
-    speaks['w'] = "woo";
+    vector<string> sounds{"aya", "ye", "woo", "ma"};
 
     for (string str : babbling) {
-        auto it = str.begin();
-        string prevStr = "";
+        string prev{""};
+        string cur{""};
 
-        while (true) {
-            char chr = *it;
-
-            if (it + 3 == str.end()) {
-                string chr3Str = string(it, it + 3);
-                if ((chr3Str == speaks['a'] || chr3Str == speaks['w']) &&
-                    chr3Str != prevStr) {
-                    answer++;
-                }
-                break;
-            } else if (it + 2 == str.end()) {
-                string chr2Str = string(it, it + 2);
-                if ((chr2Str == speaks['y'] || chr2Str == speaks['m']) &&
-                    chr2Str != prevStr) {
-                    answer++;
-                }
-                break;
-            } else if (it + 3 < str.end()) {
-                string chr3Str = string(it, it + 3);
-                string chr2Str = string(it, it + 2);
-                if ((chr3Str == speaks['a'] || chr3Str == speaks['w']) &&
-                    chr3Str != prevStr) {
-                    it = it + 3;
-                    prevStr = chr3Str;
-                } else if ((chr2Str == speaks['y'] || chr2Str == speaks['m']) &&
-                           chr2Str != prevStr) {
-                    it = it + 2;
-                    prevStr = chr2Str;
-                } else {
+        for (char c : str) {
+            cur += c;
+            auto targetIt = find(sounds.begin(), sounds.end(), cur);
+            if (targetIt == sounds.end()) {
+                continue;
+            } else {
+                if (cur == prev) {
                     break;
                 }
-            } else {
-                break;
+                prev = cur;
+                cur = "";
             }
+        }
+        if (cur.empty()) {
+            answer++;
         }
     }
 

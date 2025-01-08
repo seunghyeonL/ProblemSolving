54725b14fba88b48f8b908845d3d63bc58a6bb4f
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-01-08 12:11:11 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 3.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -3,28 +3,41 @@
 
 using namespace std;
 
-int solution(vector<int> citations)
+int pbinarySearch(int begin, int end, vector<int> &citations)
 {
+    if (begin >= end - 1)
+    {
+        return begin;
+    }
+
     int answer{};
+    int mid = (begin + end) / 2;
     int paperNum{};
 
-    while(true) {
-        answer++;
-        paperNum = 0;
-
-        for(int n : citations) {
-            if(n >= answer) {
-                paperNum++;
-            }
-        } 
-
-        if(answer == paperNum) {
-            break;
-        } else if(answer > paperNum) {
-            answer--;
-            break;
+    for (int n : citations)
+    {
+        if (n >= mid)
+        {
+            paperNum++;
         }
-    } 
+    }
 
+    if (mid == paperNum)
+    {
+        answer = mid;
+    }
+    else if (mid < paperNum)
+    {
+        answer = pbinarySearch(mid, end, citations);
+    }
+    else
+    {
+        answer = pbinarySearch(begin, mid, citations);
+    }
     return answer;
+}
+
+int solution(vector<int> citations)
+{
+    return pbinarySearch(0, 10000, citations);
 }

e00bce2adc4e2e7c4bbfc3b3a582cc809dec2956
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-01-18 13:42:46 +0900

    [level 2] Title: 롤케이크 자르기, Time: 60.02 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -14,37 +14,17 @@ int solution(vector<int> topping)
 
     for (int cur : topping)
     {
-        auto it = m2.find(cur);
-        if (it == m2.end())
-        {
-            m2.insert({cur, 1});
-        }
-        else
-        {
-            m2[cur]++;
-        }
+        m2[cur]++;
     }
 
     for (int cur : topping)
     {
-        auto it1 = m1.find(cur);
-        if (it1 == m1.end())
-        {
-            m1.insert({cur, 1});
-        }
-        else
-        {
-            m1[cur]++;
-        }
+        m1[cur]++;
 
         if (m2[cur] == 1)
-        {
             m2.erase(cur);
-        }
         else
-        {
             m2[cur]--;
-        }
 
         if (m1.size() == m2.size())
             answer++;

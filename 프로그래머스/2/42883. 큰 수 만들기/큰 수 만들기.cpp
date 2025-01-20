063cebe6a69a99b4d30779ef859354a3b74294e2
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-01-21 00:51:10 +0900

    [level 2] Title: 큰 수 만들기, Time: 1679.00 ms, Memory: 5.2 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -1,60 +1,22 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 string solution(string number, int k)
 {
-    string answer{};
-    int size = number.size();
-
-    int remainSize = size;
-    int needSize = size - k;
-
-    int idx = 0;
-    while (idx < size)
+    for (int i = 0; i < number.size() - k; i++)
     {
+        auto maxIt =
+            max_element(number.begin() + i, number.begin() + i + k + 1);
 
-        int n = number[idx];
+        k = k - distance(number.begin() + i, maxIt);
 
-        // cout << "before remainSize: " << remainSize << " needSize: " <<
-        // needSize
-        //      << '\n';
-
-        if (answer.empty())
-        {
-            // cout << "push!" << '\n';
-            answer.push_back(n);
-            --remainSize;
-            --needSize;
-            ++idx;
-            continue;
-        }
-
-        while (remainSize > needSize)
-        {
-            if (!answer.empty() && answer.back() < n)
-            {
-                // cout << "pop!" << '\n';
-                answer.pop_back();
-                ++needSize;
-                continue;
-            }
-            break;
-        }
-
-        if (needSize > 0)
-        {
-            // cout << "push!" << '\n';
-            answer.push_back(n);
-            --remainSize;
-            --needSize;
-            // cout << "after remainSize: " << remainSize
-            //      << " needSize: " << needSize << '\n';
-            
-        }
-        ++idx;
+        number.erase(number.begin() + i, maxIt);
     }
 
-    return answer;
+    number.erase(number.end() - k, number.end());
+
+    return number;
 }

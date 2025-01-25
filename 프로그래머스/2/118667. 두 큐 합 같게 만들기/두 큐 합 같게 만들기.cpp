47016afbb8b8cc2c3f283235aca5122ccc9183b4
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-01-25 15:42:18 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 4.85 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -1,83 +1,52 @@
 #include <string>
 #include <vector>
-#include <limits>
+#include <deque>
 
 using namespace std;
 
 int solution(vector<int> queue1, vector<int> queue2)
 {
     int size = queue1.size();
-    int answer = numeric_limits<int>::max();
-    vector<long long> partialSum(size * 4 + 1);
-    partialSum[0] = 0;
-    long long sum = 0;
+    int answerMax = size * 4;
+    int answer = 0;
 
+    deque<int> dq1(size);
+    long long sum1 = 0;
+    deque<int> dq2(size);
+    long long sum2 = 0;
     for (int i = 0; i < size; i++)
     {
-        partialSum[i + 1] = partialSum[i] + queue1[i];
+        sum1 += queue1[i];
+        sum2 += queue2[i];
+        dq1[i] = queue1[i];
+        dq2[i] = queue2[i];
     }
 
-    for (int i = size; i < 2 * size; i++)
+    while (sum1 != sum2 && answer < answerMax)
     {
-        partialSum[i + 1] = partialSum[i] + queue2[i - size];
-    }
-
-    for (int i = 2 * size; i < 3 * size; i++)
-    {
-        partialSum[i + 1] = partialSum[i] + queue1[i - 2 * size];
-    }
-
-    for (int i = 3 * size; i < 4 * size; i++)
-    {
-        partialSum[i + 1] = partialSum[i] + queue2[i - 3 * size];
-    }
-
-    sum = partialSum[2 * size];
-    // if (partialSum[size] * 2 == sum)
-    // {
-    //     return 0;
-    // }
-    // prints(partialSum);
-    // cout << "sum: " << sum << '\n';
-
-    for (int i = 0; i < 2 * size; i++)
-    {
-        int begin = size - 1 < i ? i : size - 1;
-        int end = 2 * size - 1 + i;
-        // cout << "i: " << i << '\n';
-
-        while (begin <= end)
+        if (sum1 < sum2)
         {
-            int mid = (begin + end) / 2;
-            // cout << "begin: " << begin << " end: " << end << " mid: " << mid
-            //      << '\n';
-            // cout << "checking value: " << partialSum[mid + 1] - partialSum[i]
-            //      << '\n';
-            if (partialSum[mid + 1] - partialSum[i] < sum / 2)
-            {
-                begin = mid + 1;
-            }
-            else if (partialSum[mid + 1] - partialSum[i] == sum / 2)
-            {
-                int curAnswer = i + mid - size + 1;
-                // cout << "curAnser: " << curAnswer << '\n';
-                // cout << "answer: " << answer << '\n';
-                answer = curAnswer < answer ? curAnswer : answer;
-                // cout << "answer: " << answer << '\n';
-                break;
-            }
-            else
-            {
-                end = mid - 1;
-            }
+            int front2 = dq2.front();
+            dq2.pop_front();
+            dq1.push_back(front2);
+            sum2 -= front2;
+            sum1 += front2;
         }
+        else
+        {
+            int front1 = dq1.front();
+            dq1.pop_front();
+            dq2.push_back(front1);
+            sum1 -= front1;
+            sum2 += front1;
+        }
+        answer++;
     }
 
-    if (answer == numeric_limits<int>::max())
+    if (answer == answerMax)
     {
         answer = -1;
     }
-
     return answer;
 }
 

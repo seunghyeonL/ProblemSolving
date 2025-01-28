34bfc312ac2778c58cd222f5517122458c44b88e
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-01-28 15:10:05 +0900

    [level 2] Title: 하노이의 탑, Time: 25.72 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <stack>
 #include <functional>
 
 using namespace std;
@@ -8,24 +7,6 @@ using namespace std;
 vector<vector<int>> solution(int n)
 {
     vector<vector<int>> answer;
-
-    vector<stack<int>> board(3, stack<int>());
-
-    for (int i = n - 1; i >= 0; --i)
-    {
-        board[0].push(i);
-    }
-
-    function<void(int, int)> move = [&](int start, int dest) -> void
-    {
-        // cout << "start: " << start << " dest: " << dest << '\n';
-        int front = board[start].top();
-        // cout << " front: " << front << '\n';
-        board[start].pop();
-        board[dest].push(front);
-
-        answer.push_back(vector<int>{start + 1, dest + 1});
-    };
 
     function<void(int, int, int)> moveHill = [&](int start, int dest,
                                                  int height) -> void
@@ -42,12 +23,12 @@ vector<vector<int>> solution(int n)
         if (height > 1)
         {
             moveHill(start, tmp, height - 1);
-            move(start, dest);
+            answer.push_back(vector<int>{start + 1, dest + 1});
             moveHill(tmp, dest, height - 1);
         }
         else
         {
-            move(start, dest);
+            answer.push_back(vector<int>{start + 1, dest + 1});
         }
     };
 

1041b76fce950f809affe8a8f3b8590f49b46119
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-02-11 10:03:49 +0900

    [level 2] Title: 디펜스 게임, Time: 18.79 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int k, vector<int> enemy) {
+    int answer = 0;
+    int waveNum = enemy.size();
+    
+    priority_queue<int, vector<int>, greater<int>> pq;
+    
+    for(int i = 0 ; i < min(k, waveNum) ; i++) 
+    {
+        pq.push(enemy[i]);
+    }
+    answer = min(k, waveNum);
+    
+    
+    
+    for(int i = min(k, waveNum) ; i < waveNum ; i++)
+    {
+        int cheatMin = pq.top();
+        
+        // cout << "i: " << i << " " << " cheatMin: " << cheatMin << " " << " enemy[i] " << enemy[i] << '\n';
+        
+        if(cheatMin < enemy[i])
+        {
+            pq.pop();
+            n -= cheatMin;
+            pq.push(enemy[i]);
+        }
+        else
+        {
+            n -= enemy[i];
+        }
+        
+        if (n < 0)
+        {
+            break;
+        }
+        
+        answer = i + 1;
+    }
+    
+    return answer;
+}

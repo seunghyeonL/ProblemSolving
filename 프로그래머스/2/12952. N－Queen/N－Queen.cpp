531370e01e21fd8ad697c5abcc04a188e41960f6
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-02-15 13:29:22 +0900

    [level 2] Title: N-Queen, Time: 141.93 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/12952. N－Queen/N－Queen.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0;
+
+    vector<pair<int, int>> queens;
+    vector<pair<int, int>> directions{
+        {1, 0}, {0, 1}, {0, -1}, {-1, 0},
+        {1, 1}, {1, -1}, {-1, -1}, {-1, 1}
+    };
+
+    auto isValid = [&](int x, int y)
+    {
+        // bool result = x >= 0 && x < n && y >= 0 && y < n;
+        bool result = true;
+        for (auto queen : queens)
+        {
+            if (queen.first == x || queen.second == y || abs(x - queen.first) == abs(y - queen.second))
+            {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    };
+
+    function<void(int)> setQueen = [&](int i)
+    {
+        if (i == n)
+        {
+            answer++;
+            return;
+        }
+        
+        for (int j = 0; j < n; j++)
+        {
+            if (!isValid(   i, j))
+            {
+                continue;
+            }
+
+            queens.emplace_back(i, j);
+            setQueen(i + 1);
+            queens.pop_back();
+        }
+    };
+
+    setQueen(0);
+    
+    return answer;
+}

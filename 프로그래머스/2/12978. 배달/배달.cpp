c2a1a1815a351142e1e115e4f2b0ce426db368a5
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-04-24 16:12:13 +0900

    [level 2] Title: 배달, Time: 0.48 ms, Memory: 4.05 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -1,70 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <limits>
 using namespace std;
 
 int solution(int N, vector<vector<int>> road, int K)
 {
     int answer = 0;
-
-    vector<vector<pair<long long, int>>> adjList(
-        N + 1, vector<pair<long long, int>>());
-    vector<long long> dist(N + 1, numeric_limits<long long>::max());
-    dist[1] = 0;
-
-    for (auto data : road)
+    
+    vector<vector<long long>> dist(N+1, vector<long long>(N+1, 5000001));
+    
+    for(int i = 1 ; i <= N ; i++)
     {
-        int u = data[0], v = data[1];
-        long long load = data[2];
-        adjList[u].push_back({load, v});
-        adjList[v].push_back({load, u});
+        dist[i][i] = 0;
     }
-
-    // for (int i = 0; i <= N; i++)
-    // {
-    //     cout << "i: " << i << " ";
-    //     printp(adjList[i]);
-    // }
-    // cout << "\n\n";
-
-    priority_queue<pair<long long, int>, vector<pair<long long, int>>,
-                   greater<pair<long long, int>>>
-        pq{greater<pair<long long, int>>()}; // 거리, 노드
-    pq.push({0, 1});
-
-    while (!pq.empty())
+    
+    for(auto roadInfo : road)
     {
-        auto [curDist, curNode] = pq.top();
-        pq.pop();
-
-        // cout << "curDist: " << curDist << " curNode: " << curNode << '\n';
-        // prints(dist);
-
-        if (curDist != dist[curNode])
-            continue;
-
-        for (auto [nextDistDiff, nextNode] : adjList[curNode])
+        int a = roadInfo[0];
+        int b = roadInfo[1];
+        long long c = roadInfo[2];
+        
+        dist[a][b] = min(dist[a][b], c);
+        dist[b][a] = min(dist[b][a], c);
+    }
+    
+    for(int k = 1 ; k <= N ; k++)
+    {
+        for(int i = 1 ; i <= N ; i++)
         {
-            // if (nextNode == -1)
-            //     continue;
-            if (dist[curNode] + nextDistDiff < dist[nextNode])
+            for(int j = 1 ; j <= N ; j++)
             {
-                dist[nextNode] = dist[curNode] + nextDistDiff;
-                // cout << "nextDist: " << dist[nextNode]
-                //      << " nextNode: " << nextNode << '\n';
-                pq.push({dist[nextNode], nextNode});
+                
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
             }
         }
     }
-
-    // prints(dist);
-
-    for (int i = 1; i <= N; i++)
+    
+    for(int i = 1 ; i <= N ; i++)
     {
-        if (dist[i] <= K)
-            answer++;
+        if(dist[1][i] <= K) answer++;
     }
-
     return answer;
 }

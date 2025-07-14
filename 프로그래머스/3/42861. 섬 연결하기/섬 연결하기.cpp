ddffcd88d1dee94b5bf3ccad64a6b93824891dd2
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-07-14 21:51:09 +0900

    [level 3] Title: 섬 연결하기, Time: 0.05 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -1,15 +1,40 @@
 #include <string>
 #include <vector>
-#include <tuple>
-#include <queue>
 #include <functional>
 
 using namespace std;
 
 int solution(int n, vector<vector<int>> costs) {
-    int result = 0;
+    vector<int> p(n, -1);
     
-    vector<vector<pair<int, int>>> adj(n); // {비용, 노드}
+    function<int(int)> findRoot = [&](int u) -> int
+    {
+        if(p[u] < 0) return u;
+        
+        return p[u] = findRoot(p[u]);
+    };
+    
+    auto unionSet = [&](int u, int v) -> bool
+    {
+        u = findRoot(u);
+        v = findRoot(v);
+        
+        if(u == v) return false;
+        
+        if(p[u] < p[v])
+        {
+            p[v] = u; 
+        }
+        else
+        {
+            p[u] = v;
+        }
+        return true;
+    };
+    
+    sort(costs.begin(), costs.end(), [](auto a, auto b){ return a[2] < b[2]; });
+    
+    int result = 0;
     
     for(auto edgeCost : costs)
     {
@@ -17,47 +42,11 @@ int solution(int n, vector<vector<int>> costs) {
         int v = edgeCost[1];
         int cost = edgeCost[2];
         
-        adj[u].push_back({cost, v});
-        adj[v].push_back({cost, u});
-    }
-    
-    priority_queue<
-        tuple<int, int, int>, 
-        vector<tuple<int, int, int>>, 
-        greater<tuple<int, int, int>>
-            > pq;
-    
-    vector<bool> connected(n, false);
-    int connectedNum  = 0;
-    
-    connected[0] = true;
-    connectedNum++;
-    for(auto [cost, v] : adj[0])
-    {
-        pq.push({cost, 0, v});
-    }
-    
-    while(connectedNum < n)
-    {
-        auto [cost, u, v] = pq.top();
-        pq.pop();
-        
-        // 이미 연결된 상태 건너뛰기
-        if(connected[v]) continue;
-        
-        // 연결하기
-        result += cost;
-        connected[v] = true;
-        connectedNum++;
-        
-        // pq에 새로 연결된 노드의 edge들 추가
-        for(auto [nextCost, nextV] : adj[v])
+        if (unionSet(u, v))
         {
-            if(connected[nextV]) continue;
-            pq.push({nextCost, v, nextV});
+            result += cost;
         }
     }
-    
     
     return result;
 }

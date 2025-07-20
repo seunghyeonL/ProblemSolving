4703fe32b6d88ac7cee5938e8cd2bd988021c055
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-07-20 15:22:17 +0900

    [level 3] Title: 순위, Time: 1.07 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -6,64 +6,33 @@ int solution(int n, vector<vector<int>> results)
 {
     int answer = 0;
 
-    // 단방향 연결리스트 2개
-    vector<vector<int>> winAdj(n + 1);  // 내가 이긴놈 연결
-    vector<vector<int>> loseAdj(n + 1); // 나를 이긴놈 연결
+    vector<vector<int>> winTable(n + 1, vector<int>(n + 1, 0));
 
     for (auto result : results)
     {
         int winner = result[0];
         int loser = result[1];
 
-        winAdj[winner].push_back(loser);
-        loseAdj[loser].push_back(winner);
+        winTable[winner][loser] = 1;
     }
 
-    vector<int> visited(n + 1);
-
-    function<int(int)> getWinClusterSize = [&](int player)
-    {
-        int result = 1;
-        visited[player] = 1;
-        for (int loserForMe : winAdj[player])
-        {
-            if (!visited[loserForMe])
-            {
-                result += getWinClusterSize(loserForMe);
-            }
-        }
-
-        return result;
-    };
-
-    function<int(int)> getLoseClusterSize = [&](int player)
-    {
-        int result = 1;
-        visited[player] = 1;
-        for (int winnerForMe : loseAdj[player])
-        {
-            if (!visited[winnerForMe])
-            {
-                result += getLoseClusterSize(winnerForMe);
-            }
-        }
-
-        return result;
-    };
+    for (int k = 1; k <= n; k++)
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= n; j++)
+                if (winTable[i][k] && winTable[k][j])
+                    winTable[i][j] = 1;
 
     for (int i = 1; i <= n; i++)
     {
-        int winSize = getWinClusterSize(i) - 1;
-        fill(visited.begin(), visited.end(), 0);
-        int loseSize = getLoseClusterSize(i) - 1;
-        fill(visited.begin(), visited.end(), 0);
-
-        if (winSize + loseSize == n - 1)
+        int cnt = 0;
+        for (int j = 1; j <= n; j++)
         {
-            answer++;
+            if (winTable[i][j] || winTable[j][i])
+                cnt++;
         }
+        if (cnt == n - 1)
+            answer++;
     }
-
 
     return answer;
 }

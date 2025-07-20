4d3deb889e6f826368ac39f8894f3a45997d434f
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-07-20 15:49:56 +0900

    [level 2] Title: 배달, Time: 0.31 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -5,40 +5,39 @@ using namespace std;
 
 int solution(int N, vector<vector<int>> road, int K)
 {
-    using P = pair<long long, int>;
-		
     int answer = 0;
+    // 거리, 노드
+    using P = pair<long long, int>;
 
-    vector<vector<P>> adjList(
-        N + 1, vector<P>());
-    vector<long long> dist(N + 1, 500001);
-    dist[1] = 0;
+    vector<vector<P>> adj(N + 1);
 
-    for (auto data : road)
+    for (auto roadInfo : road)
     {
-        int u = data[0], v = data[1];
-        long long load = data[2];
-        adjList[u].push_back({load, v});
-        adjList[v].push_back({load, u});
+        int u = roadInfo[0];
+        int v = roadInfo[1];
+        int c = roadInfo[2];
+
+        adj[u].push_back({c, v});
+        adj[v].push_back({c, u});
     }
 
-    priority_queue<P, vector<P>, greater<P>> pq; // 거리, 노드
-    pq.push({0, 1});
+    vector<long long> dist(N + 1, 1e11);
 
+    priority_queue<P, vector<P>, greater<P>> pq;
+    pq.push({0, 1});
+    dist[1] = 0;
+    
     while (!pq.empty())
     {
-        auto [curDist, curNode] = pq.top();
+        auto [cC, cV] = pq.top();
         pq.pop();
 
-        if (curDist != dist[curNode])
-            continue;
-
-        for (auto [nextDistDiff, nextNode] : adjList[curNode])
+        for (auto [nC, nV] : adj[cV])
         {
-            if (dist[curNode] + nextDistDiff < dist[nextNode])
+            if (dist[nV] > dist[cV] + nC)
             {
-                dist[nextNode] = dist[curNode] + nextDistDiff;
-                pq.push({dist[nextNode], nextNode});
+                dist[nV] = dist[cV] + nC;
+                pq.push({dist[nV], nV});
             }
         }
     }

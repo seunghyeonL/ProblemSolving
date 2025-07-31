dbb46beca8f2a2d1267b2c9228006b48b30a9a5c
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-07-31 20:53:44 +0900

    [level 3] Title: 110 옮기기, Time: 69.65 ms, Memory: 23.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.cpp
@@ -0,0 +1,61 @@
+#include <string>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+vector<string> solution(vector<string> s)
+{
+    vector<string> answer;
+    string s110 = "110";
+
+    auto compare110 = [&](auto it)
+    {
+        return *it == '1' && *(it + 1) == '1' && *(it + 2) == '0';
+    };
+
+    auto solve = [&](string str)
+    {
+        int cnt110 = 0;
+        string remain;
+
+        for (char c : str)
+        {
+            remain.push_back(c);
+            int size = remain.size();
+
+            if (size >= 3 && remain[size - 3] == '1' && remain[size - 2] == '1' && remain[size - 1] == '0')
+            {
+                cnt110++;
+                remain.erase(remain.end() - 3, remain.end());
+            }
+        }
+
+        string result{};
+        auto place110 = remain.begin();
+
+        for (auto it = remain.begin(); it != remain.end(); ++it)
+        {
+            if (*it == '0')
+            {
+                place110 = it + 1;
+            }
+        }
+
+        result += string(remain.begin(), place110);
+        for (int _ = 0; _ < cnt110; _++)
+        {
+            result += s110;
+        }
+        result += string(place110, remain.end());
+
+        answer.push_back(result);
+    };
+
+    for (string el : s)
+    {
+        solve(el);
+    }
+
+    return answer;
+}

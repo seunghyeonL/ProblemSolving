8fddb8460585066e9def9e3bc6c4699f640a4214
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-08-08 18:23:36 +0900

    [level 2] Title: 땅따먹기, Time: 13.78 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/1880. 땅따먹기/땅따먹기.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int> > land)
+{
+    int N = land.size();
+    
+    vector<vector<int>> dp(N, vector<int>(4, 0)); // dp[i][j] : (i, j)를 마지막으로 밟을 때의 최대값
+    
+    for (int i = 0 ; i < 4 ; i++)
+    {
+        dp[0][i] = land[0][i];
+    }
+    
+    for (int i = 1 ; i < N ; i++)
+    {
+        for (int j = 0 ; j < 4 ; j++)
+        {
+            for (int k = 0 ; k < 4 ; k++)
+            {
+                if (k == j) continue;
+                dp[i][j] = max(dp[i][j], dp[i - 1][k] + land[i][j]);
+            }
+        }
+    }
+
+    int answer = 0;
+    for (auto el : dp[N - 1])
+    {
+        answer = max(answer, el);
+    }
+    
+    return answer;
+}

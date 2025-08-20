1a9edbac2dba5423a86577a495f46d1702658ca3
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-08-20 12:29:34 +0900

    [level 4] Title: 지형 이동, Time: 23.26 ms, Memory: 9.66 MB -BaekjoonHub

---
+++ 프로그래머스/4/62050. 지형 이동/지형 이동.cpp
@@ -0,0 +1,103 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+
+using namespace std;
+
+int solution(vector<vector<int>> land, int height)
+{
+    using T = tuple<int, int, int>; // 비용, 노드, 노드
+    int answer = 0;
+    int N = land.size();
+    // edge를 만들고(오른쪽, 아래쪽으로)
+    // cost는 max(abs(h1 - h2) - height, 0)
+    // 크루스칼로 mst만들때 비용 찾기
+
+    auto isValid = [N](int x, int y)
+    { return x >= 0 && x < N && y >= 0 && y < N; };
+
+    auto getCost = [&](int h1, int h2)
+    { return max(abs(h1 - h2) - height, 0); };
+
+    vector<T> edges;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            int v = j + N * i;
+            int h = land[i][j];
+            // 아래쪽
+            if (isValid(i + 1, j))
+            {
+                int nv1 = j + N * (i + 1);
+                int h1 = land[i + 1][j];
+                edges.emplace_back(getCost(h, h1), v, nv1);
+            }
+
+            // 오른쪽
+            if (isValid(i, j + 1))
+            {
+                int nv2 = j + 1 + N * i;
+                int h2 = land[i][j + 1];
+                edges.emplace_back(getCost(h, h2), v, nv2);
+            }
+        }
+    }
+
+    vector<int> uf(N * N, -1);
+
+    function<int(int)> findRoot = [&](int u)
+    {
+        if (uf[u] < 0)
+            return u;
+
+        return uf[u] = findRoot(uf[u]);
+    };
+
+    auto unionSet = [&](int u, int v)
+    {
+        u = findRoot(u);
+        v = findRoot(v);
+
+        if (u == v)
+            return false;
+
+        if (uf[u] > uf[v])
+        {
+            uf[u] = v;
+        }
+        else if (uf[u] == uf[v])
+        {
+            uf[u] = v;
+            uf[v]--;
+        }
+        else
+        {
+            uf[v] = u;
+        }
+
+        return true;
+    };
+
+    sort(edges.begin(), edges.end());
+    // Printc<vector<T>, Printt> printc;
+    // printc(edges);
+
+    int cnt = 0;
+    for (auto [c, u, v] : edges)
+    {
+        if (unionSet(u, v))
+        {
+            if (c > 0)
+                answer += c + height;
+            cnt++;
+        }
+
+        if (cnt == N * N - 1)
+            break;
+    }
+
+    return answer;
+}

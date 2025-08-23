892e0dbbedc2f7642e7c40c7cd5cd1a7dbf045d1
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-08-23 11:10:19 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -8,70 +8,38 @@ int solution(int n, vector<vector<int>> wires)
 {
     int answer = n;
 
-    vector<vector<int>> adjList(n + 1, vector<int>{0});
+    vector<vector<int>> adj(n + 1);
 
     for (auto wire : wires)
     {
-        int node1 = wire[0];
-        int node2 = wire[1];
-        adjList[node1].push_back(node2);
-        adjList[node2].push_back(node1);
+        int u = wire[0];
+        int v = wire[1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
 
-    vector<bool> isVisited(n + 1, false);
-    isVisited[0] = true;
+    vector<int> subtreeSize(n + 1);
 
-    function<int(int)> dfs = [&](int node) -> int
+    function<void(int)> setSize = [&](int u)
     {
-        // prints(isVisited);
-        int result = 1;
-        isVisited[node] = true;
+        subtreeSize[u] = 1;
 
-        for (int nextNode : adjList[node])
+        for (int v : adj[u])
         {
-            if (!isVisited[nextNode])
+            if (!subtreeSize[v])
             {
-                result += dfs(nextNode);
+                setSize(v);
+                subtreeSize[u] += subtreeSize[v];
             }
         }
-
-        return result;
     };
 
-    for (auto wire : wires)
+    setSize(1);
+
+    for (int i = 1; i <= n; i++)
     {
-        int node1 = wire[0];
-        int node2 = wire[1];
-        adjList[node1].erase(
-            remove(adjList[node1].begin(), adjList[node1].end(), node2),
-            adjList[node1].end());
-        adjList[node2].erase(
-            remove(adjList[node2].begin(), adjList[node2].end(), node1),
-            adjList[node2].end());
-
-        vector<int> curLengths;
-
-        for (int i = 1; i <= n; i++)
-        {
-            if (!isVisited[i])
-            {
-                curLengths.push_back(dfs(i));
-            }
-        }
-
-        int curAnswer = curLengths[0] < curLengths[1]
-                            ? curLengths[1] - curLengths[0]
-                            : curLengths[0] - curLengths[1];
-
-        if (curAnswer < answer)
-            answer = curAnswer;
-
-        adjList[node1].push_back(node2);
-        adjList[node2].push_back(node1);
-        for (int i = 1; i <= n; i++)
-        {
-            isVisited[i] = false;
-        }
+        answer = min(answer, abs(n - 2 * subtreeSize[i]));
     }
 
     return answer;

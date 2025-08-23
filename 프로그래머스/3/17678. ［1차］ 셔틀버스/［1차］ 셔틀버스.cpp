6232beb66b39ee2c38017cea003bfbbae8aad613
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-08-23 10:21:09 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 1.49 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.cpp
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.cpp
@@ -36,26 +36,22 @@ string getTimeString(int minute)
     int h = minute / 60;
     int m = minute % 60;
 
-    string hs{};
-    string ms{};
+		string result{};
 
-    hs.push_back('0' + h / 10);
-    hs.push_back('0' + h % 10);
+    result.push_back('0' + h / 10);
+    result.push_back('0' + h % 10);
+		result.push_back(':');
+    result.push_back('0' + m / 10);
+    result.push_back('0' + m % 10);
 
-    ms.push_back('0' + m / 10);
-    ms.push_back('0' + m % 10);
-
-    string result{};
-    result += hs;
-    result += ':';
-    result += ms;
     return result;
 }
 
 string solution(int n, int t, int m, vector<string> timetable)
 {
     using P = pair<int, bool>; // 정류장 도착시간, 콘인지 여부
-    // 탑승 시각(분)으로 이분탐색
+    // 정류장 도착시간(분)으로 이분탐색
+    // 정류장 도착시간에 따라 콘이 셔틀버스를 탈 수 있는지 없는지 여부를 판별
 
     vector<int> bustimes;
     int firstBustime = getMinute("09:00");

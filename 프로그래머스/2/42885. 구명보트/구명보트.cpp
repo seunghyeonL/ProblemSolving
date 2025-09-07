a7d73afbe542f1bdb932797a0f9d39c5323d177a
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-09-07 10:14:08 +0900

    [level 2] Title: 구명보트, Time: 1.93 ms, Memory: 4.71 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -5,35 +5,25 @@
 
 using namespace std;
 
-int solution(vector<int> people, int limit) {
+int solution(vector<int> people, int limit)
+{
     int answer = 0;
-    
+
     sort(people.begin(), people.end());
-    
-    int left = 0;
-    int right = people.size() - 1;
-    int boat = 0;
-    int cnt = 0;
-    
+
+    auto left = people.begin();
+    auto right = people.end() - 1;
+
     while (left <= right)
     {
-        while (left <= right && cnt < 2 && boat + people[right] <= limit)
-        {
-            boat += people[right--];
-            cnt++;
-        }
+        int boat = 0;
+        boat += *(right--);
         
-        while (left <= right && cnt < 2 && boat + people[left] <= limit)
-        {
-            boat += people[left++];
-            cnt++;
-        }
-        
+        if (left <= right && boat + *left <= limit) 
+            boat += *(left++);
+
         answer++;
-        boat = 0;
-        cnt = 0;
     }
-    
-    
+
     return answer;
 }

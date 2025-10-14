5c972411fd79dcc7f9e832600ccbf3458007d48b
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-10-14 22:53:53 +0900

    [Gold V] Title: 수 이어 쓰기 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.cc
+++ 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.cc
@@ -32,31 +32,22 @@ int main(int argc, char const *argv[])
 
     int len = 1;       // 수 길아
     long long num = 9; // len 길이의 수 개수
+    int cur = 1;
 
     while (K - num * len > 0)
     {
+        cur += num;
         K -= num * len;
         len++;
         num *= 10;
     }
 
-    int ans = -1;
-    bool found = false;
-    for (int i = power(10, len - 1); i <= N && !found; i++)
-    {
-        string si = to_string(i);
+    int target = cur + (K - 1) / len;
 
-        for (int j = 0; j < len; j++)
-        {
-            // cout << si[j];
-            if (--K == 0)
-            {
-                ans = si[j] - '0';
-                found = true;
-                break;
-            }
-        }
-        // cout << '\n';
+    int ans = -1;
+    if (target <= N)
+    {
+        ans = to_string(target)[(K - 1) % len] - '0';
     }
 
     cout << ans;

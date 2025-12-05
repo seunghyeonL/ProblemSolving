a0930c2ae133ef9bfb57a46a8ae3ed01e45582e6
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-05 10:58:36 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.16 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using T = tuple<int, int, int>;
+
+const int INF = 1e9;
+int dist[25][25][4];
+int N;
+
+bool is_valid(int x, int y)
+{
+    return x >= 0 && x < N && y >= 0 && y < N;
+}
+
+vector<pair<int, int>> moves
+{
+    {0, 1},
+    {0, -1},
+    {1, 0},
+    {-1, 0},
+};
+
+int solution(vector<vector<int>> board) {
+    N = board.size();
+    for (int i = 0 ; i < N ; i++)
+        for (int j = 0 ; j < N ; j++)
+            for (int dir = 0 ; dir < 4 ; dir++)
+                dist[i][j][dir] = INF;
+    
+    queue<T> q; // x, y, dir
+    
+    dist[0][0][0] = 0;
+    dist[0][0][2] = 0;
+    q.emplace(0, 0, 0);
+    q.emplace(0, 0, 2);
+    
+    while (!q.empty())
+    {
+        auto [cx, cy, cd] = q.front();
+        q.pop();
+        
+        for (int nd = 0 ; nd < 4 ; nd++)
+        {
+            auto [dx, dy] = moves[nd];
+            int nx = cx + dx;
+            int ny = cy + dy;
+          
+            int w = (cd == nd ? 100 : 600);
+                
+            if (is_valid(nx, ny) && !board[nx][ny] && dist[nx][ny][nd] > dist[cx][cy][cd] + w)
+            {
+                dist[nx][ny][nd] = dist[cx][cy][cd] + w;
+                q.emplace(nx, ny, nd);
+            }
+        }
+    }
+    
+    return min(dist[N - 1][N - 1][0], dist[N - 1][N - 1][2]);
+}

03ee1ec600f424a5ca5ee5f7c50450f0b0f8f5a7
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-06 21:42:15 +0900

    [level 3] Title: 카드 짝 맞추기, Time: 102.97 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/72415. 카드 짝 맞추기/카드 짝 맞추기.cpp
+++ 프로그래머스/3/72415. 카드 짝 맞추기/카드 짝 맞추기.cpp
@@ -1,186 +1,138 @@
-#include <string>
-#include <vector>
-#include <queue>
-#include <unordered_map>
-#include <functional>
-
+#include <bits/stdc++.h>
 using namespace std;
+using T = tuple<int, int, int>;
 
-int solution(vector<vector<int>> board, int r, int c)
+const int INF = 1e9;
+const int N = 4;
+set<int> card_set;
+vector<pair<int, int>> card_pos[7];
+
+bool is_card[N][N];
+
+// cost[sx][sy][ex][ey] : {sx, sy} -> {ex, ey} 최소 조작 횟수 
+int cost[N][N][N][N];
+
+vector<pair<int, int>> moves
 {
-    using P = pair<int, int>;
-    int answer = 0;
-    int N = 4;
-    /*
-    카드 종류, 좌표 매핑해놓기
+    {1, 0},
+    {-1, 0},
+    {0, 1},
+    {0, -1},
+};
 
-    다음에 없앨 카드 하나 정하고
-    뭐부터 없앨지 순서 하나 정해서
-    백트래킹
-    */
+bool is_valid(int x, int y)
+{
+    return x >= 0 && x < N && y >= 0 && y < N;
+}
 
-    // Printc<vector<P>, Printp<P>> printc;
+void init_cost(int sx, int sy)
+{
+    for (int i = 0 ; i < N ; i++)
+        for (int j = 0 ; j < N ; j++)
+            cost[sx][sy][i][j] = INF;
+}
 
-    unordered_map<int, vector<P>> cards;
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            if (board[i][j])
-                cards[board[i][j]].emplace_back(i, j);
-
-    // for (auto [n, v] : cards)
-    // {
-    //     for (auto [i, j] : v)
-    //     {
-    //         cout << '(' << n << ": (" << i << ", " << j << "))";
-    //     }
-    //     cout << "\n";
-    // }
-
-    vector<P> moves{
-        {1, 0}, {0, 1}, {-1, 0}, {0, -1} // down, right, up, left
-    };
-
-    auto isValid = [N](int x, int y)
-    { return x >= 0 && x < N && y >= 0 && y < N; };
-
-    auto getNextPoints = [&](int x, int y) -> vector<P>
+void dijkstra(int sx, int sy)
+{
+    init_cost(sx, sy);
+    
+    auto* c_cost = cost[sx][sy];
+    
+    priority_queue<T, vector<T>, greater<T>> pq;
+    
+    c_cost[sx][sy] = 0;
+    pq.emplace(0, sx, sy);
+    
+    while (!pq.empty())
     {
-        vector<P> result;
-
+        auto [d, cx, cy] = pq.top();
+        pq.pop();
+        
+        if (d > c_cost[cx][cy]) continue;
+        
         for (auto [dx, dy] : moves)
         {
-            int nx = x + dx;
-            int ny = y + dy;
+            int nx1 = cx + dx;
+            int ny1 = cy + dy;
 
-            if (!isValid(nx, ny))
+            if (is_valid(nx1, ny1) && c_cost[nx1][ny1] > d + 1)
             {
-                continue;
+                c_cost[nx1][ny1] = d + 1;
+                pq.emplace(d + 1, nx1, ny1);
             }
-
-            // 그냥 이동
-            result.emplace_back(nx, ny);
-            if (board[nx][ny])
-                continue;
-
-            // ctrl 이동
-            while (isValid(nx + dx, ny + dy))
+            
+            int nx2 = nx1;
+            int ny2 = ny1;
+            
+            while (is_valid(nx2 + dx, ny2 + dy) && !is_card[nx2][ny2])
             {
-                nx += dx;
-                ny += dy;
-
-                if (board[nx][ny])
-                {
-                    result.emplace_back(nx, ny);
-                    break;
-                }
-
-                // 끝점
-                if (!isValid(nx + dx, ny + dy))
-                {
-                    result.emplace_back(nx, ny);
-                }
+                nx2 += dx;
+                ny2 += dy;
+            }
+            
+            if (is_valid(nx2, ny2) && c_cost[nx2][ny2] > d + 1)
+            {
+                c_cost[nx2][ny2] = d + 1;
+                pq.emplace(d + 1, nx2, ny2);
             }
         }
+    }
+}
 
-        return result;
-    };
-
-    auto bfs = [&](int sx, int sy, int tx, int ty) -> int
+// board 상태, 커서 위치 cx, cy 일때 다 없애기까지 필요한 최소 조작 수
+int dfs(int cx, int cy)
+{
+    // 어떤 카드를 없앨지 고르고 
+    // 없앤 상태로 dfs 돌리고
+    // 돌린 dfs 값과 고른 카드 없애는 비용 더해서 
+    // 최솟값 반환
+    
+    if (card_set.empty()) 
     {
-        vector<vector<int>> dist(4, vector<int>(4, -1));
-        queue<P> q;
-
-        dist[sx][sy] = 0;
-        q.push({sx, sy});
-
-        while (!q.empty())
-        {
-            auto [cx, cy] = q.front();
-            q.pop();
-
-            if (cx == tx && cy == ty)
-            {
-                break;
-            }
-
-            for (auto [nx, ny] : getNextPoints(cx, cy))
-            {
-                if (dist[nx][ny] == -1)
-                {
-                    dist[nx][ny] = dist[cx][cy] + 1;
-                    q.push({nx, ny});
-                }
-            }
-        }
-
-        return dist[tx][ty];
-    };
-
-    vector<bool> removed(7);
-
-    function<int(int, int)> solve = [&](int cx, int cy) -> int
+        return 0;
+    }
+    
+    dijkstra(cx, cy);
+    
+    int ret = INF;
+    
+    for (int card : card_set)
     {
-        int result = 1e9;
+        auto [tx1, ty1] = card_pos[card][0];
+        auto [tx2, ty2] = card_pos[card][1];
+        
+        dijkstra(tx1, ty1);
+        dijkstra(tx2, ty2);
+        
+        // 엔터 두번
+        int path_cost1 = cost[cx][cy][tx1][ty1] + cost[tx1][ty1][tx2][ty2] + 2;
+        int path_cost2 = cost[cx][cy][tx2][ty2] + cost[tx2][ty2][tx1][ty1] + 2;
+        
+        is_card[tx1][ty1] = false;        
+        is_card[tx2][ty2] = false;
+        card_set.erase(card);
+        
+        ret = min(ret, dfs(tx1, ty1) + path_cost2);
+        ret = min(ret, dfs(tx2, ty2) + path_cost1);
+        
+        card_set.insert(card);
+        is_card[tx1][ty1] = true;        
+        is_card[tx2][ty2] = true;
+    }
+    
+    return ret;
+}
 
-        // cout << "cx: " << cx << "cy: " << cy << '\n';
-        for (auto [card, points] : cards)
+int solution(vector<vector<int>> board, int r, int c) {
+    for (int i = 0 ; i < N ; i++)
+        for (int j = 0 ; j < N ; j++)
         {
-            if (removed[card])
-                continue;
-
-            auto [tx1, ty1] = points[0];
-            auto [tx2, ty2] = points[1];
-
-            // 첫번째부터 제거
-            int result1 = 0;
-            result1 += bfs(cx, cy, tx1, ty1) + 1;
-            board[tx1][ty1] = 0;
-            result1 += bfs(tx1, ty1, tx2, ty2) + 1;
-            board[tx2][ty2] = 0;
-
-            removed[card] = true;
-            result1 += solve(tx2, ty2);
-            removed[card] = false;
-
-            board[tx1][ty1] = card;
-            board[tx2][ty2] = card;
-
-            result = min(result, result1);
-
-            // 두번째부터 제거
-            int result2 = 0;
-            result2 += bfs(cx, cy, tx2, ty2) + 1;
-            board[tx2][ty2] = 0;
-            result2 += bfs(tx2, ty2, tx1, ty1) + 1;
-            board[tx1][ty1] = 0;
-
-            removed[card] = true;
-            result2 += solve(tx1, ty1);
-            removed[card] = false;
-
-            board[tx1][ty1] = card;
-            board[tx2][ty2] = card;
-
-            result = min(result, result2);
+            if (!board[i][j]) continue;
+            is_card[i][j] = true;
+            
+            card_set.insert(board[i][j]);
+            card_pos[board[i][j]].emplace_back(i, j);
         }
-
-        // cout << cx << ' ' << cy << '\n';
-        // cout << (result == 1e9 ? 0 : result) << '\n';
-
-        return result == 1e9 ? 0 : result;
-    };
-
-    // cout << bfs(r, c, 0, 0) << '\n';
-    // cout << bfs(r, c, 3, 1) << '\n';
-    // cout << bfs(r, c, 3, 3) << '\n';
-
-    // cout << '\n';
-
-    // cout << bfs(0, 0, r, c) << '\n';
-    // cout << bfs(3, 1, r, c) << '\n';
-    // cout << bfs(3, 3, r, c) << '\n';
-
-    // printc(getNextPoints(r, c));
-
-    return solve(r, c);
+    
+    return dfs(r, c);
 }

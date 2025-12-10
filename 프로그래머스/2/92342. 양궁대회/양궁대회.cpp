2a96e4d44a8b2d9605ff1a30672028d42e35de92
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-10 09:19:29 +0900

    [level 2] Title: 양궁대회, Time: 0.05 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.cpp
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.cpp
@@ -1,75 +1,98 @@
-#include <string>
-#include <vector>
-#include <functional>
-#include <utility>
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> solution(int n, vector<int> info)
+// 각 점수를 가져가고/안가져가고
+// 경우로 나눠서 백트래킹
+// 최대 점수차 구하기
+
+const int NMX = 10;
+const int I = 11;
+vector<int> arr;
+
+bool comp(const vector<int>& a, const vector<int>& b)
 {
-    int maxDiff = 0;
-    vector<int> answer{-1};
-    vector<int> apeachInfo(info.begin(), info.end());
-    vector<int> lionInfo(11, 0);
-
-    auto calculateScore = [&]() -> pair<int, int>
+    int sz = a.size();
+    for (int i = sz - 1 ; i >= 0 ; i--)
     {
-        int lionScore = 0;
-        int apeachScore = 0;
+        if (a[i] != b[i]) return a[i] < b[i];
+    }
+    
+    return false;
+}
 
-        for (int i = 0; i < 11; i++)
-        {
-            if (lionInfo[i] > apeachInfo[i])
-                lionScore += 10 - i;
-            else if (apeachInfo[i] > 0)
-                apeachScore += 10 - i;
-        }
-        return {lionScore, apeachScore};
-    };
-
-    function<void(int, int)> dfs = [&](int curIdx, int shootCnt) -> void
+// info idx, 남은 화살 개수, 점수차, 최대 점수차, 최대 점수차 일때 화살 기록,현재 화살 기록
+void bt(int idx, int n, int diff, int& mx_diff, vector<int>& ans, vector<int>& cur)
+{
+    // pruning
+    if (diff + accumulate(arr.begin() + idx, arr.begin() + I, 0, 
+                          [](int acc, int cur){ return acc + 10 - cur; }) < mx_diff) return;
+    
+    if (idx == I)
     {
-        if(shootCnt == n)
+        // 점수차가 mx_diff 이상이고 현재 조합이 낮은 점수에 더 많이 쐈을떄
+        if (diff > mx_diff || diff == mx_diff && comp(ans, cur))
         {
-            auto [lionScore, apeachScore] = calculateScore();
+            cout << diff << '\n';
+            for (int el : cur)
+            {
+                cout << el << ' ';
+            }
+            cout << '\n';
             
-            if(maxDiff < lionScore - apeachScore)
-            {
-                maxDiff = lionScore - apeachScore;
-                answer = lionInfo;
-            }
-            else if(maxDiff == lionScore - apeachScore)
-            {
-                for(int i = 10 ; i >= 0 ; i--)
-                {
-                    if(answer[i] > lionInfo[i]) return;
-                    else if(answer[i] < lionInfo[i])
-                    {
-                        answer = lionInfo;
-                        break;
-                    }
-                    
-                }   
-            }
-            return;
+            mx_diff = diff;
+            ans = cur;
         }
         
-        for(int i = curIdx ; i <= 10 ; i++)
+        return;
+    }
+    
+    
+    if (idx < I - 1)
+    {
+        if (arr[idx] > 0)
         {
-            int arrowNum = apeachInfo[i] + 1;
-            if(arrowNum > n - shootCnt) {
-                if(i < 10) continue;
-                else arrowNum = n - shootCnt;
+            if (arr[idx] + 1 <= n)
+            {
+                cur[idx] = arr[idx] + 1;
+                bt(idx + 1, n - arr[idx] - 1, diff + (10 - idx), mx_diff, ans, cur);
+                cur[idx] = 0;
             }
-            
-            lionInfo[i] = arrowNum;
-            dfs(i + 1, shootCnt + arrowNum);
-            lionInfo[i] = 0;
+
+            bt(idx + 1, n, diff - (10 - idx), mx_diff, ans, cur);
         }
-    };
+        else
+        {
+            if (1 <= n)
+            {
+                cur[idx] = 1;
+                bt(idx + 1, n - 1, diff + (10 - idx), mx_diff, ans, cur);
+                cur[idx] = 0;
+            }
 
-    dfs(0, 0);
+            bt(idx + 1, n, diff, mx_diff, ans, cur);
+        }
+    }
+    else
+    {
+        // idx == I - 1 == 10 ; 점수 0
+        cur[idx] = n;
+        bt(idx + 1, 0, diff, mx_diff, ans, cur);
+        cur[idx] = 0;
+    }
+}
 
-    return answer;
+vector<int> solution(int n, vector<int> info) 
+{
+    // n = 3;
+    // info = {0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1};
+    arr = info;
+    
+    vector<int> ans(I, 1);
+    vector<int> cur(I);
+    int mx_diff = -1000;
+    
+    bt(0, n, 0, mx_diff, ans, cur);
+    
+    if (mx_diff <= 0) return {-1};
+    else return ans;
 }

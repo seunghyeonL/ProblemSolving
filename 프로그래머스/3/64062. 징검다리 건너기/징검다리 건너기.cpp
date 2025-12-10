c68d107c57314fa07ae58496e96ce7f4953b3a70
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-10 13:34:12 +0900

    [level 3] Title: 징검다리 건너기, Time: 8.21 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -1,55 +1,57 @@
-#include <string>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int solution(vector<int> stones, int k)
+// 건널 수 있는 사람은 ans 이상이면 못건너고 ans 이하면 건널 수 있음
+// 이분탐색
+
+const int LMT = 200'000'000;
+int K, L;
+vector<int> i_stones; // 초기 상태
+vector<int> c_stones;
+
+// n명이 건널 수 있는가?
+bool check(int n)
 {
-    int size = stones.size();
-
-    /*
-        건널 수 있는 사람이 n명 이라고 할때 가능한지 여부로 이분탐색
-        n이 불가능한 n이면 n+1도 불가능
-        n이 가능한 n이면 n-1도 가능
-
-        각 요소에 n - 1 을 뺐을때 연속하는 0 이하인 개수 찾기
-    */
-
-    // n명이 다리를 건널 수 있는가?
-    auto check = [&](int n)
+    // 모든 돌에서 n - 1을 뺐을때 최대 연속 0 이하 개수가 K보다 작아야 건널 수 있음
+    c_stones = i_stones;
+    
+    for (int& val : c_stones)
+        val -= (n - 1);
+    
+    int mx_0_len = 0;
+    // i : 연속 0 이하 이전 idx, j : 현재 idx
+    for (int i = -1, j = 0 ; j <= L ; j++)
     {
-        int zeroCount = 0;
-
-        for (int i = 0; i < size; i++)
+        if (j == L)
         {
-            if (stones[i] - (n - 1) <= 0)
-                zeroCount++;
-            else
-                zeroCount = 0;
-
-            if (zeroCount == k)
-                return false;
+            mx_0_len = max(mx_0_len, j - i - 1);
+            break;
         }
-
-        return true;
-    };
-
-    int left = 1;
-    int right = 1e9;
-
-    while (left <= right)
-    {
-        int mid = (left + right) / 2;
-
-        if (check(mid))
-        {
-            left = mid + 1;
-        }
-        else
-        {
-            right = mid - 1;
-        }
+        
+        if (c_stones[j] <= 0) continue;
+        
+        mx_0_len = max(mx_0_len, j - i - 1);
+        i = j;
     }
+    
+    return mx_0_len < K;
+}
 
-    return right;
+int solution(vector<int> stones, int k) {
+    K = k;
+    L = stones.size();
+    i_stones = stones;
+    
+    int l = 0, r = LMT;
+    while (l <= r)
+    {
+        int m = (l + r) / 2;
+        
+        if (check(m)) 
+            l = m + 1;
+        else
+            r = m - 1;
+    }
+    
+    return r;
 }

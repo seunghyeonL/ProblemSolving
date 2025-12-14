62a0d1dda1273068915d8abb9fc8de6b14449bdc
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-15 07:54:17 +0900

    [level 3] Title: 야근 지수, Time: 31.27 ms, Memory: 3.77 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.cpp
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.cpp
@@ -1,82 +1,36 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <numeric>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-long long solution(int n, vector<int> works)
-{
-    using ll = long long;
-    ll answer = 0;
+// A1 + ... + An == c 일때
+// A1^2 + ... + An^2 최소가 되는 A1, ... , An찾기
+// 그리디
+// A1, ..., An이 최대한 똑같도록 평탄하게 
 
-    if (n >= accumulate(works.begin(), works.end(), 0))
+long long solution(int n, vector<int> works) {
+    long long ans = 0;
+    
+    int sz = works.size();
+    
+    priority_queue<int> pq(works.begin(), works.end());
+    
+    for (int i = 0 ; i < n ; i++)
     {
-        return 0;
+        int mx = pq.top();
+        pq.pop();
+        
+        if (mx == 0) 
+            return 0;
+        
+        pq.push(mx - 1);
     }
-
-    sort(works.begin(), works.end(), greater<int>());
-
-    /*
-    N 으로 works의 최댓값을 얼마나 줄일 수 있는가
-    N으로 가능한 works[i]가 최댓값이 되는 최대 i 찾기
-    이분탐색
-    */
-
-    auto check = [&](int idx)
+    
+    while (!pq.empty())
     {
-        ll need = 0;
-        for (int i = 0; i < idx; i++)
-        {
-            need += works[i] - works[idx];
-        }
-        return need <= n;
-    };
-
-    int left = 0;
-    int right = works.size() - 1;
-
-    while (left <= right)
-    {
-        int mid = (left + right) / 2;
-
-        if (check(mid))
-        {
-            left = mid + 1;
-        }
-        else
-        {
-            right = mid - 1;
-        }
+        long long cur = pq.top();
+        pq.pop();
+        
+        ans += cur * cur;
     }
-
-    int tidx = right;
-
-    int need = 0;
-    for (int i = 0; i < tidx; i++)
-    {
-        need += works[i] - works[tidx];
-        works[i] = works[tidx];
-    }
-
-    int remain = n - need;
-
-    while (remain > 0)
-    {
-        for (int i = 0; i <= tidx; i++)
-        {
-            if (remain > 0)
-            {
-                works[i]--;
-                remain--;
-            }
-        }
-    }
-
-    for (int work : works)
-    {
-        answer += work * work;
-    }
-
-    return answer;
+    
+    return ans;
 }

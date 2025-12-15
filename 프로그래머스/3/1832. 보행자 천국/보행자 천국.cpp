729335b44ffd26bbe459f9e6a1298ded6e8c8e07
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-15 11:27:01 +0900

    [level 3] Title: 보행자 천국, Time: 20.47 ms, Memory: 8.5 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.cpp
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.cpp
@@ -1,49 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// x, y, 들어오는방향 을 상태로 dp
+
 const int MOD = 20170805;
-const int MX = 500;
-// dp[i][j][dir] : {i, j} 위치에 dir 방향으로 들어오는 경로 개수
-// dir ->  0 : 오른쪽, 1 : 아래쪽
-int dp[MX + 1][MX + 1][2];
+const int NMX = 500;
 
 int N, M;
+vector<vector<int>> board;
 
-bool is_valid(int x, int y)
-{
-    return x >= 0 && x < N && y >= 0 && y < M;
-}
-
-vector<pair<int, int>> moves
-{
-    {0, 1},
-    {1, 0},
-};
+// 0 : 아래로, 1 : 오른쪽으로
+int dp[NMX][NMX][2];
 
 // 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
-int solution(int m, int n, vector<vector<int>> city_map) 
-{    
+int solution(int m, int n, vector<vector<int>> _city_map) 
+{
     N = m;
     M = n;
-    
+    board = _city_map;
     memset(dp, 0, sizeof(dp));
     
-    dp[0][1][0] = 1;
-    dp[1][0][1] = 1;
+    dp[0][1][1] = 1;
+    dp[1][0][0] = 1;
     
     for (int i = 0 ; i < N ; i++)
         for (int j = 0 ; j < M ; j++)
         {
             if (i == 0 && j == 0) continue;
+            if (board[i][j] == 1) continue;
             
-            if (city_map[i][j] == 0)
+            // 아래로 이동
+            if (i < N - 1) 
             {
-                dp[i + 1][j][1] = dp[i][j + 1][0] = (dp[i][j][0] + dp[i][j][1]) % MOD;
+                if (board[i][j] == 0)
+                    dp[i + 1][j][0] += dp[i][j][0] + dp[i][j][1];
+                else
+                    dp[i + 1][j][0] += dp[i][j][0];
+
+                
+                dp[i + 1][j][0] %= MOD;
             }
-            else if (city_map[i][j] == 2)
+            
+            // 오른쪽으로 이동
+            if (j < M - 1)
             {
-                dp[i + 1][j][1] = dp[i][j][1];
-                dp[i][j + 1][0] = dp[i][j][0];
+                if (board[i][j] == 0)
+                    dp[i][j + 1][1] += dp[i][j][0] + dp[i][j][1];
+                else
+                    dp[i][j + 1][1] += dp[i][j][1];
+
+                
+                dp[i][j + 1][1] %= MOD;
             }
         }
     

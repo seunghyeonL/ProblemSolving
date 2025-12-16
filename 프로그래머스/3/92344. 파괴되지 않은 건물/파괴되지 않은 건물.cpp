2005b0abc82da5142fcc0e560fcf40c5e4368e57
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-16 23:34:02 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 27.92 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.cpp
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.cpp
@@ -1,49 +1,48 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-const int MX = 1000;
+const int NMX = 1000;
 int N, M;
-int D[MX + 1][MX + 1];
+int D[NMX + 1][NMX + 1];
 
 int solution(vector<vector<int>> board, vector<vector<int>> skill) {
+    memset(D, 0, sizeof(D));
     N = board.size();
     M = board[0].size();
     
-    for (const auto& s_info : skill)
+    for (auto& sk : skill)
     {
-        int t = s_info[0];
-        int r1 = s_info[1];
-        int c1 = s_info[2];
-        int r2 = s_info[3];
-        int c2 = s_info[4];
-        int w = s_info[5];
-        
-        w *= (t == 1 ? -1 : 1);
-        
-        D[r1][c1] += w;
-        D[r1][c2 + 1] -= w;
-        D[r2 + 1][c1] -= w;
-        D[r2 + 1][c2 + 1] += w;
+        int t = sk[0];
+        int r1 = sk[1];
+        int c1 = sk[2];
+        int r2 = sk[3];
+        int c2 = sk[4];
+        int d = sk[5];
+            
+        int sign = t == 1 ? -1 : 1;
+        D[r1][c1] += d * sign;
+        D[r1][c2 + 1] -= d * sign;
+        D[r2 + 1][c1] -= d * sign;
+        D[r2 + 1][c2 + 1] += d * sign;
     }
     
     for (int i = 0 ; i < N ; i++)
-        for (int j = 1 ; j < M ; j++)
-            D[i][j] += D[i][j - 1];
+    {
+        inclusive_scan(D[i], D[i] + M, D[i]);
+    }
     
     for (int j = 0 ; j < M ; j++)
+    {
         for (int i = 1 ; i < N ; i++)
             D[i][j] += D[i - 1][j];
-    
-    for (int i = 0 ; i < N ; i++)
-        for (int j = 0 ; j < M ; j++)
-            board[i][j] += D[i][j];
+    }
     
     int ans = 0;
     for (int i = 0 ; i < N ; i++)
         for (int j = 0 ; j < M ; j++)
-            if (board[i][j] > 0) 
+            if (board[i][j] + D[i][j] > 0)
                 ans++;
     
+
     return ans;
 }

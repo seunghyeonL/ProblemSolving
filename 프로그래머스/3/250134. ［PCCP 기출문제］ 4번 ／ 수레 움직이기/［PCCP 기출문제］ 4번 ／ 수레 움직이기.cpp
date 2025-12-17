201008d42aaeb247e4c1aa4488eab10b098d964d
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-17 21:20:59 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수레 움직이기, Time: 0.23 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.cpp
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.cpp
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+using P = pair<int, int>;
 
+// red : 0, blue : 1
+const int INF = 1e9;
 const int MX = 4;
 int N, M;
+vector<vector<int>> maze;
+P ev0, ev1;
 
-// 0 : blue, 1 : red
-pair<int, int> sv[2];
-pair<int, int> ev[2];
-// pair<int, int> cv[2];
-
-vector<vector<int>> board;
-bool vis[2][MX][MX];
+bool vis0[MX][MX];
+bool vis1[MX][MX];
 
 vector<pair<int, int>> moves
 {
-    {0, 1},
-    {0, -1},
     {1, 0},
     {-1, 0},
+    {0, 1},
+    {0, -1},
 };
 
 bool is_valid(int x, int y)
@@ -25,109 +25,111 @@ bool is_valid(int x, int y)
     return x >= 0 && x < N && y >= 0 && y < M;
 }
 
-void dfs(pair<int, int> cv0, pair<int, int> cv1, int ct, int& ans)
+void dfs(P cv0, P cv1, int turn, int& mn_turn)
 {
-    if (cv0 == ev[0] && cv1 == ev[1])
+    if (turn >= mn_turn)
+        return;
+    
+    if (cv0 == ev0 && cv1 == ev1)
     {
-        ans = min(ans, ct);
+        mn_turn = min(mn_turn, turn);
         return;
     }
     
     auto [cx0, cy0] = cv0;
     auto [cx1, cy1] = cv1;
     
-    vector<pair<int, int>> nvs0;
-    vector<pair<int, int>> nvs1;
+    vector<pair<int, int>> nv0s;
+    vector<pair<int, int>> nv1s;
     
-    if (cv0 != ev[0])
+    if (cv0 != ev0)
     {
-        for (auto [dx0, dy0] : moves)
+        for (auto [dx, dy] : moves)
         {
-            int nx0 = cx0 + dx0;
-            int ny0 = cy0 + dy0;
-            auto nv0 = make_pair(nx0, ny0);
+            int nx0 = cx0 + dx;
+            int ny0 = cy0 + dy;
             
-            if (!is_valid(nx0, ny0) || vis[0][nx0][ny0] || board[nx0][ny0] == 5)
+            if (!is_valid(nx0, ny0) || maze[nx0][ny0] == 5 || vis0[nx0][ny0])
                 continue;
             
-            nvs0.push_back(nv0);
+            nv0s.emplace_back(nx0, ny0);
         }
     }
     else
-    {
-        nvs0.push_back({cx0, cy0});
-    }
+        nv0s.push_back(cv0);
     
-    if (cv1 != ev[1])
+    if (cv1 != ev1)
     {
-        for (auto [dx1, dy1] : moves)
+        for (auto [dx, dy] : moves)
         {
-            int nx1 = cx1 + dx1;
-            int ny1 = cy1 + dy1;
-            auto nv1 = make_pair(nx1, ny1);
-
-            if (!is_valid(nx1, ny1) || vis[1][nx1][ny1] || board[nx1][ny1] == 5)
+            int nx1 = cx1 + dx;
+            int ny1 = cy1 + dy;
+            
+            if (!is_valid(nx1, ny1) || maze[nx1][ny1] == 5 || vis1[nx1][ny1])
                 continue;
-
-            nvs1.push_back(nv1);
+            
+            nv1s.emplace_back(nx1, ny1);
         }
     }
     else
-    {
-        nvs1.push_back({cx1, cy1});
-    }
+        nv1s.push_back(cv1);
     
-    for (const auto& nv0 : nvs0)
+    for (const auto& nv0 : nv0s)
     {
-        for (const auto& nv1 : nvs1)
+        for (const auto& nv1 : nv1s)
         {
-            if (nv0 == nv1) continue;
-            if (nv0 == cv1 && nv1 == cv0) continue;
+            if (nv0 == nv1)
+                continue;
             
-            vis[0][nv0.first][nv0.second] = true;
-            vis[1][nv1.first][nv1.second] = true;
+            if (nv0 == cv1 && nv1 == cv0)
+                continue;
             
-            dfs(nv0, nv1, ct + 1, ans);
+            auto [nx0, ny0] = nv0;
+            auto [nx1, ny1] = nv1;
             
-            vis[0][nv0.first][nv0.second] = false;
-            vis[1][nv1.first][nv1.second] = false;
+            vis0[nx0][ny0] = true;
+            vis1[nx1][ny1] = true;
+            
+            dfs(nv0, nv1, turn + 1, mn_turn);
+            
+            vis0[nx0][ny0] = false;
+            vis1[nx1][ny1] = false;
         }
     }
+    
 }
 
-int solution(vector<vector<int>> maze) {
+int solution(vector<vector<int>> _maze) 
+{
+    maze = _maze;
     N = maze.size();
     M = maze[0].size();
-    board = maze;
+    memset(vis0, false, sizeof(vis0));
+    memset(vis1, false, sizeof(vis1));
+    
+    P sv0, sv1;
     
     for (int i = 0 ; i < N ; i++)
         for (int j = 0 ; j < M ; j++)
         {
             if (maze[i][j] == 1)
-            {
-                sv[1] = {i, j};
-                // cv[1] = {i, j};
-            }
+                sv0 = {i, j};
             else if (maze[i][j] == 2)
-            {
-                sv[0] = {i, j};
-                // cv[0] = {i, j};
-            }
+                sv1 = {i, j};
             else if (maze[i][j] == 3)
-            {
-                ev[1] = {i, j};
-            }
+                ev0 = {i, j};
             else if (maze[i][j] == 4)
-            {
-                ev[0] = {i, j};
-            }
+                ev1 = {i, j};
         }
     
-    vis[0][sv[0].first][sv[0].second] = true;
-    vis[1][sv[1].first][sv[1].second] = true;
+    int ans = INF;
     
-    int ans = 100;
-    dfs(sv[0], sv[1], 0, ans);
+    auto [sx0, sy0] = sv0;
+    auto [sx1, sy1] = sv1;
     
-    return (ans == 100 ? 0 : ans);
+    vis0[sx0][sy0] = true;
+    vis1[sx1][sy1] = true;
+    dfs(sv0, sv1, 0, ans);
+
+    return ans == INF ? 0 : ans;
 }

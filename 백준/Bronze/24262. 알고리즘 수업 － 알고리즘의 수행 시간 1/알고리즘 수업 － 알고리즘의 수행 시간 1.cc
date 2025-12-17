53560e7262c964834c61a1b8a27e64be9d65b378
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-17 23:30:20 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // #include <bits/stdc++.h>
+    // using namespace std;
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // ifstream inputFileStream("input.txt");
+    
+    cout << 1 << '\n';
+    cout << 0 << '\n';
+    
+    // inputFileStream.close();
+    return 0;
+}

e359ea14cae9dbf1feb08cea5d378b588c0ca929
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-18 12:35:40 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.02 ms, Memory: 4.44 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.cpp
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.cpp
@@ -1,42 +1,141 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int solution(vector<string> board) {
-    int ONum = 0, XNum = 0;
-    string CompleteLine = "";
+// 안되는 조건
+// X 개수 < O 개수 - 1 || x 개수 > O 개수
+// O가 완성되었는데 O 개수 != X 개수 + 1
+// X가 완성되었는데 O 개수 != X 개수
+// 완성된 쌍이 여러개
 
-    for (string row : board) {
-        for (char c : row) {
-            if (c == 'O') ONum++;
-            if (c == 'X') XNum++;
+// O -> 0, X -> 1
+
+const int N = 3;
+
+int solution(vector<string> board) 
+{
+    int num_0 = 0;
+    int num_1 = 0;
+    int end_0 = 0;
+    int end_1 = 0;
+    
+    for (int i = 0 ; i < N ; i++)
+        for (int j = 0 ; j < N ; j++)
+        {
+            if (board[i][j] == 'O')
+                num_0++;
+            else if (board[i][j] == 'X')
+                num_1++;
+        }
+    
+    if (num_1 < num_0 - 1 || num_1 > num_0)
+        return 0;
+    
+    // 행 체크
+    for (int i = 0 ; i < N ; i++)
+    {
+        int cn0 = 0;
+        int cn1 = 0;
+        for (int j = 0 ; j < N ; j++)
+        {
+            if (board[i][j] == 'O')
+                cn0++;
+            else if (board[i][j] == 'X')
+                cn1++;
+        }
+        
+        if (cn0 == N)
+        {
+            end_0++;
+        }
+        
+        if (cn1 == N)
+        {
+            end_1++;
         }
     }
-
-    if (ONum != XNum && ONum != XNum + 1) return 0;
-
-    vector<string> lines = { board[0], board[1], board[2] };
     
-    for (int i = 0; i < 3; i++) {
-        string col = { board[0][i], board[1][i], board[2][i] };
-        lines.push_back(col);
+    // 열 체크
+    for (int j = 0 ; j < N ; j++)
+    {
+        int cn0 = 0;
+        int cn1 = 0;
+        for (int i = 0 ; i < N ; i++)
+        {
+            if (board[i][j] == 'O')
+                cn0++;
+            else if (board[i][j] == 'X')
+                cn1++;
+        }
+        
+        if (cn0 == N)
+        {
+            end_0++;
+        }
+        
+        if (cn1 == N)
+        {
+            end_1++;
+        }
     }
-
-    lines.push_back({ board[0][0], board[1][1], board[2][2] });
-    lines.push_back({ board[0][2], board[1][1], board[2][0] });
-
-    int oWin = 0, xWin = 0;
-
-    for (string line : lines) {
-        if (line == "OOO") oWin++;
-        if (line == "XXX") xWin++;
+    
+    // 대각 체크
+    {
+        int cn0 = 0;
+        int cn1 = 0;
+        for (int i = 0 ; i < N ; i++)
+        {
+            if (board[i][i] == 'O')
+                cn0++;
+            else if (board[i][i] == 'X')
+                cn1++;
+        }
+        
+        if (cn0 == N)
+        {
+            end_0++;
+        }
+        
+        if (cn1 == N)
+        {
+            end_1++;
+        }
     }
-
-    if (oWin > 0 && xWin > 0) return 0;
-    if (oWin > 0 && ONum != XNum + 1) return 0;
-    if (xWin > 0 && ONum != XNum) return 0;
+    
+    {
+        int cn0 = 0;
+        int cn1 = 0;
+        for (int i = 0 ; i < N ; i++)
+        {
+            /*
+             2 0
+             1 1
+             0 2
+            */
+            if (board[N - 1 - i][i] == 'O')
+                cn0++;
+            else if (board[N - 1 - i][i] == 'X')
+                cn1++;
+        }
+        
+        if (cn0 == N)
+        {
+            end_0++;
+        }
+        
+        if (cn1 == N)
+        {
+            end_1++;
+        }
+    }
+    
+    if (end_0 && end_1)
+        return 0;
+    
+    if (end_0 && (num_0 != num_1 + 1))
+        return 0;
+    
+    if (end_1 && (num_0 != num_1))
+        return 0;
 
     return 1;
 }

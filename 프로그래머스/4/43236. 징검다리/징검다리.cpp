630ace5e7640e3e2c8686e2700d3b5552678158e
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-18 19:58:07 +0900

    [level 4] Title: 징검다리, Time: 6.26 ms, Memory: 5.29 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.cpp
+++ 프로그래머스/4/43236. 징검다리/징검다리.cpp
@@ -1,54 +1,65 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int solution(int distance, vector<int> rocks, int n) {
-    int answer = 0;
+// 구간을 need개로 합칠때 최소구간 길이의 최댓값
+// 최소 길이가 특정 값 보다 작으면 항상 가능
+// 최소 길이가 특정 값 보다 크면 항상 불가능
+// -> 이분탐색
+
+int need;
+int N; // 구간 개수
+vector<int> D;
+
+// mn_len보다 큰 최소 길이로 구간을 만들어 나갈때
+// 개수가 need개 이상인가 (need개보다 많을 경우 합치면 더 길어지니 need개로 만들 수 있음)
+bool check(int mn_len)
+{
+    int pn = 0;
+    int cl = 0;
     
-    sort(rocks.begin(), rocks.end());
-    int size = rocks.size();
-    
-    int left = 1;
-    int right = distance;
-    
-    while (left <= right)
+    for (int i = 0 ; i < N ; i++)
     {
-        // 지점 사이 최솟값 중 최댓값
-        int mid = (left + right) / 2;
+        cl += D[i];
         
-        int removed = 0;
-        int prev = 0;
-        
-        for (int i = 0 ; i < rocks.size() ; i++)
+        if (cl >= mn_len)
         {
-            if (rocks[i] - prev < mid) 
-            {
-                removed++;
-            }
-            else
-            {
-                prev = rocks[i];
-            }
+            pn++;
+            cl = 0;
         }
-        
-        if (distance - prev < mid)
-        {
-            removed++;
-        }
-        
-        if (removed <= n)
-        {
-            left = mid + 1;
-            answer = mid;
-        }
-        else
-        {
-            right = mid - 1;
-        }
-        
     }
     
-    return answer;
+    if (pn >= need)
+        return true;
+    else 
+        return false;
+}
+
+int solution(int distance, vector<int> rocks, int n) 
+{
+    N = rocks.size() + 1;
+    need = N - n;
+    
+    rocks.push_back(0);
+    rocks.push_back(distance);
+    
+    sort(rocks.begin(), rocks.end());
+    
+    for (int i = 0 ; i < N ; i++)
+        D.push_back(rocks[i + 1] - rocks[i]);
+    
+    int l = 1, r = 1e9;
+    while (l <= r)
+    {
+        int m = (l + r) / 2;
+        
+        if (check(m))
+            l = m + 1;
+        else
+            r = m - 1;
+    }
+    
+    for (int i = 0 ; i < N ; i++)
+        
+    
+    return r;
 }

a2c95af1202d43aadd5dcfd58bf6722f67421d1b
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-19 11:13:07 +0900

    [level 2] Title: 교점에 별 만들기, Time: 4.89 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.cpp
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.cpp
@@ -1,76 +1,72 @@
-#include <string>
-#include <vector>
-#include <iostream>
-#include <set>
-#include <limits>
-#include <tuple>
-
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 
-tuple <int, int, bool> getIntersection(const vector<int>& line1, const vector<int>& line2)
+/*
+    초기 상태 - o
+    경계 처리 - o
+    이벤트 순서
+    오버플로우 - o
+*/
+
+vector<pair<ll, ll>> star_pos;
+
+vector<string> solution(vector<vector<int>> line) 
 {
-    int A = line1[0];
-    int B = line1[1];
-    int C = line2[0];
-    int D = line2[1];
-    int E = line1[2];
-    int F = line2[2];
+    int N = line.size();
     
-    if((ll)A*D - (ll)B*C == 0) return {0, 0, false};
-    if(((ll)B*F - (ll)E*D) % ((ll)A*D - (ll)B*C) != 0) return {0, 0, false};
-    if(((ll)E*C - (ll)A*F) % ((ll)A*D - (ll)B*C) != 0) return {0, 0, false};
-    
-    return {((ll)B*F - (ll)E*D)/ll((ll)A*D - (ll)B*C), ((ll)E*C - (ll)A*F)/((ll)A*D - (ll)B*C), true};
-}
-
-vector<string> solution(vector<vector<int>> line) {
-    int size = line.size();
-    
-    set<pair<int, int>> s;
-    
-    int maxX = numeric_limits<int>::min();
-    int maxY = numeric_limits<int>::min();
-    int minX = numeric_limits<int>::max();
-    int minY = numeric_limits<int>::max();
-    
-    for(int i = 0 ; i < size - 1; i++)
+    for (int i = 0 ; i < N ; i++)
     {
-        for(int j = i + 1 ; j < size ; j++)
+        for (int j = i + 1 ; j < N ; j++)
         {
-            auto [x, y, intersectFlag] = getIntersection(line[i], line[j]);
-            if(!intersectFlag) continue;
+            ll a = line[i][0];
+            ll b = line[i][1];
+            ll e = line[i][2];
             
-            if(x > maxX) maxX = x;
-            if(x < minX) minX = x;
-            if(y > maxY) maxY = y;
-            if(y < minY) minY = y;
-
-            s.insert({x, y});
+            ll c = line[j][0];
+            ll d = line[j][1];
+            ll f = line[j][2];
+            
+            ll D = a * d - b * c;
+            ll A = b * f - e * d;
+            ll B = e * c - a * f;
+            
+            if (D == 0)
+                continue;
+            
+            if (A % D != 0 || B % D != 0)
+                continue;
+            
+            star_pos.emplace_back(A / D, B / D);
+            
         }
     }
     
-    int N = maxX - minX + 1;
-    int M = maxY - minY + 1;
+    ll mn_x = 1e18;
+    ll mn_y = 1e18;
+    ll mx_x = -1e18;
+    ll mx_y = -1e18;
     
-    cout << "X: " << minX << " " << maxX << '\n';
-    cout << "Y: " << minY << " " << maxY << '\n';
-    
-
-    int minXDiffZero = 0 - minX;
-    int minYDiffZero = 0 - minY;
-    
-    vector<string> answer(M, string(N, '.'));
-    
-    for(auto [x, y] : s)
+    for (auto [x, y] : star_pos)
     {
-        x += minXDiffZero;
-        y += minYDiffZero;
-        
-        y = M - y - 1;
-        
-        answer[y][x] = '*';
+        mn_x = min(mn_x, x);
+        mx_x = max(mx_x, x);
+        mn_y = min(mn_y, y);
+        mx_y = max(mx_y, y);
     }
     
-    return answer;
+    int sz_x = mx_x - mn_x + 1;
+    int sz_y = mx_y - mn_y + 1;
+    
+    // x, y 반전
+    vector<string> ans(sz_y, string(sz_x, '.'));
+    
+    for (auto [x, y] : star_pos)
+    {
+        ans[y - mn_y][x - mn_x] = '*';
+    }
+    
+    reverse(ans.begin(), ans.end());
+    
+    return ans;
 }

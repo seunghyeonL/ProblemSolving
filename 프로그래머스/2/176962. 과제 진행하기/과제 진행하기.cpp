520891187b158a5ea71a0b88688d91260240a74e
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-19 13:05:07 +0900

    [level 2] Title: 과제 진행하기, Time: 0.88 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.cpp
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.cpp
@@ -1,127 +1,54 @@
-#include <string>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<string> strSplit(string str, char seperator = ' ')
+// 24h == 1440m
+// 분당 시뮬레이션 하면서
+// 끊기는 과제를 stack에 넣고 관리 (최근에 멈춘 과제부터 시작하기 때문)
+
+int get_minute(const string& str)
 {
-    vector<string> result;
-
-    string buffer{};
-
-    for (char chr : str)
-    {
-        if (chr == seperator)
-        {
-            result.push_back(buffer);
-            buffer.clear();
-            continue;
-        }
-
-        buffer.push_back(chr);
-    }
-
-    if (!buffer.empty())
-    {
-        result.push_back(buffer);
-    }
-
-    return result;
+    int h = stoi(str.substr(0, 2));
+    int m = stoi(str.substr(3, 2));
+    
+    return h * 60 + m;
 }
 
-vector<string> solution(vector<vector<string>> plans) {
-    struct Plan 
+vector<string> solution(vector<vector<string>> plans) 
+{
+    int N = plans.size();
+    
+    sort(plans.begin(), plans.end(), [](const auto& a, const auto& b){
+        return a[1] < b[1];
+    });
+    
+    // 이름, 남은 시간
+    stack<pair<string, int>> st;
+    vector<string> ans;
+    
+    
+    for (int pi = 0, t = 0 ; pi < N || !st.empty() ; t++)
     {
-        string name;
-        int start;
-        int playTime;
-
-        bool operator<(const Plan& rhs) const
+        if (!st.empty())
         {
-            return start > rhs.start;
-        }
-    };
+            auto& [cn, rt] = st.top();
 
-    vector<string> answer;
-    int size = plans.size();
-    
-    priority_queue<Plan> q;
-    stack<Plan> s;
-    
-    for(auto plan : plans)
-    {
-        string name = plan[0];
-        int playTime = stoi(plan[2]);
-        string time = plan[1];
-        auto splitedTime = strSplit(time, ':');
+            if (--rt == 0)
+            {
+                ans.push_back(cn);
+                st.pop();
+            }
+        }
         
-        int start = stoi(splitedTime[0]) * 60 + stoi(splitedTime[1]);
-        
-        q.push({name, start, playTime});
+        if (pi < N)
+        {
+            const auto& plan = plans[pi];
+            if (t == get_minute(plan[1]))
+            {
+                st.emplace(plan[0], stoi(plan[2]));
+                pi++;
+            }
+        }
     }
     
-    Plan curPlan = q.top();
-    int curTime = curPlan.start;
-    q.pop();
-
-    // Prints<vector<string>> prints;
-    
-    while (!q.empty() || !s.empty())
-    {
-        // prints(answer);
-        auto& [curName, curStart, curPlayTime] = curPlan;
-        // cout << "cur: " << curName << " " << curTime << " " << curPlayTime << '\n';
-            
-        if (!q.empty())
-        {
-            Plan nextPlan = q.top();
-            auto& [nextName, nextStart, nextPlayTime] = nextPlan;
-            // cout << "next: " << nextName << " " << nextStart << " " << nextPlayTime << '\n';
-            if (curTime + curPlayTime > nextStart)
-            {
-                curPlayTime = curTime + curPlayTime - nextStart;
-                curTime = nextStart;
-                
-                s.push(curPlan);
-                curPlan = nextPlan;
-                q.pop();
-            }
-            else
-            {
-                if (!s.empty())
-                {
-                    curTime += curPlayTime;
-                    answer.push_back(curName);
-                
-                    curPlan = s.top();
-                    s.pop();
-                }
-                else
-                {
-                    curTime = nextStart;
-                    answer.push_back(curName);
-                
-                    curPlan = nextPlan;
-                    q.pop();
-                }
-            }
-                
-        }
-        else if (!s.empty())
-        {
-            curTime += curPlayTime;
-            answer.push_back(curName);
-                
-            curPlan = s.top();
-            s.pop();
-        }
-    }
-
-    answer.emplace_back(curPlan.name);
-
-    
-    return answer;
+    return ans;
 }

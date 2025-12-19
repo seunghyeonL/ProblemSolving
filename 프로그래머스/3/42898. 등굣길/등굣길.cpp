3170fbb3eab9cbed066d5a720934b2c63732565b
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-19 16:10:50 +0900

    [level 3] Title: 등굣길, Time: 0.09 ms, Memory: 3.79 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -1,9 +1,44 @@
-#include <string>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int solution(int m, int n, vector<vector<int>> puddles) {
-    int answer = 0;
-    return answer;
+// 오른쪽 또는 아래쪽으로만 이동
+
+const int MOD = 1'000'000'007;
+const int NMX = 100;
+int N, M;
+
+// 1 : 웅덩이
+int board[NMX + 1][NMX + 1];
+// (0, 0) -> (x, y) 최단경로 개수
+int dp[NMX + 1][NMX + 1];
+
+int solution(int m, int n, vector<vector<int>> puddles) 
+{
+    N = m;
+    M = n;
+    
+    memset(dp, 0, sizeof(dp));
+    memset(board, 0, sizeof(board));
+    
+    for (const auto& pud : puddles)
+    {
+        int x = pud[0];
+        int y = pud[1];
+        
+        board[x][y] = 1;
+    }
+    
+    dp[1][1] = 1;
+    
+    for (int i = 1 ; i <= N ; i++)
+        for (int j = 1 ; j <= M ; j++)
+        {
+            if (board[i][j])
+                continue;
+            
+            if (i < N) dp[i + 1][j] = (dp[i + 1][j] + dp[i][j]) % MOD;
+            if (j < M) dp[i][j + 1] = (dp[i][j + 1] + dp[i][j]) % MOD;
+        }
+    
+    return dp[N][M];
 }

584ea81df7b4b298caac258112654a20f8b5fe39
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-19 15:19:08 +0900

    [level 2] Title: 디펜스 게임, Time: 1326.48 ms, Memory: 45.2 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
@@ -3,46 +3,57 @@ using namespace std;
 using ll = long long;
 
 // enemy 길이가 1000000 -> 완전탐색 불가
-// for e : enemy
-// priority_queue에 e를 넣고 
-// e를 합치며
-// 안쓰면 죽을때 그동안 e 중 최대에 무적권 사용
 
-// 죽거나 enemy가 끝날때 까지
+// 막을수 있는 최대 라운드 : ans
+// ans 이하 라운드에서 막을 수 있는가? -> true
+// ans 이하 라운드에서 막을 수 있는가? -> false
 
-int solution(int n, int k, vector<int> enemy) 
+
+// mx_rn 라운드 동안 막을 수 있는가?
+// <=> 
+// 범위에서 제일 큰 K개의 라운드에 우선권을 썼을때 가능한가?
+
+int N;
+int K;
+vector<int> enemy;
+
+bool check(int mx_rn)
 {
-    int N = n;
-    int K = k;
+    priority_queue<int> pq(enemy.begin(), enemy.begin() + mx_rn);
     
-    priority_queue<int> pq;
+    ll cw = accumulate(enemy.begin(), enemy.begin() + mx_rn, 0LL);
     
-    int ans = 0;
-    ll acc = 0;
-    
-    for (int e : enemy)
+    for (int i = 0 ; i < K ; i++)
     {
-        pq.push(e);
-        acc += e;
+        if (pq.empty())
+            break;
         
-        if (acc > N)
-        {
-            while (acc > N && K > 0 && !pq.empty())
-            {
-                K--;
-                acc -= pq.top();
-                pq.pop();
-            }
-            
-            // 막기 실패
-            if (acc > N)
-                break;
-        }
-        
-        // 이번 라운드 성공
-        ans++;
+        cw -= pq.top();
+        pq.pop();
     }
     
+    if (cw <= N)
+        return true;
+    else 
+        return false;
+}
+
+
+int solution(int n, int k, vector<int> _enemy) 
+{
+    N = n;
+    K = k;
+    enemy = _enemy;
     
-    return ans;
+    int l = 0, r = enemy.size();
+    while (l <= r)
+    {
+        int m = (l + r) / 2;
+        if (check(m))
+            l = m + 1;
+        else 
+            r = m - 1;
+    }
+    
+    return r;
 }

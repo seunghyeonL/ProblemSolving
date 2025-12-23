4f85a55fa84aeaca4ad8e3e0925410ac0a37dd3e
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-23 14:25:11 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 0 ms, Memory: 3000 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // #include <bits/stdc++.h>
+    // using namespace std;
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // ifstream inputFileStream("input.txt");
+
+    int N;
+    cin >> N;
+    cout << string(N, 'a');
+
+    // inputFileStream.close();
+    return 0;
+}

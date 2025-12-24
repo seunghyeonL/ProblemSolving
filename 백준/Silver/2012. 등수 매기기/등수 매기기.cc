f965ed8ef2e1e6e000e9f5ecddf503be215def36
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-24 17:22:19 +0900

    [Silver III] Title: 등수 매기기, Time: 76 ms, Memory: 3976 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // #include <bits/stdc++.h>
+    // using namespace std;
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // ifstream inputFileStream("input.txt");
+
+    int N;
+    cin >> N;
+
+    vector<int> v(N);
+    for (int i = 0; i < N; i++)
+        cin >> v[i];
+
+    sort(v.begin(), v.end());
+
+    long long ans = 0;
+    for (int i = 0; i < N; i++)
+        ans += abs((i + 1) - v[i]);
+
+    cout << ans << '\n';
+
+    // inputFileStream.close();
+    return 0;
+}

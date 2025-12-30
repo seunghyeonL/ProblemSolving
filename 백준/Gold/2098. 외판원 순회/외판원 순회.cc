069be70176ecd1d9c881872cd7efe440afe1d2da
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2025-12-30 12:38:58 +0900

    [Gold I] Title: 외판원 순회, Time: 36 ms, Memory: 10724 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.cc
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.cc
@@ -8,55 +8,6 @@ int adj[NMX + 1][NMX + 1];
 // dp[현재까지 방문 노드 mask, 현재 노드] = 최소 비용
 int dp[1 << (NMX + 1)][NMX + 1];
 
-int dijkstra(int sv)
-{
-    fill(&dp[0][0], &dp[0][0] + (1 << (NMX + 1)) * (NMX + 1), INF);
-
-    // 현재까지 비용, 현재 마스크, 현재 노드
-    using T = tuple<int, int, int>;
-    priority_queue<T, vector<T>, greater<T>> pq;
-
-    pq.emplace(0, 1 << sv, sv);
-    dp[1 << sv][sv] = 0;
-
-    int ret = INF;
-
-    while (!pq.empty())
-    {
-        auto [cost, mask, cv] = pq.top();
-        pq.pop();
-
-        if (dp[mask][cv] < cost)
-            continue;
-
-        if (mask == (1 << (N + 1)) - 2)
-        {
-            if (adj[cv][sv])
-                ret = min(ret, cost + adj[cv][sv]);
-
-            continue;
-        }
-
-        for (int nv = 1; nv <= N; nv++)
-        {
-            if (!adj[cv][nv])
-                continue;
-
-            if (mask >> nv & 1)
-                continue;
-
-            int n_mask = mask | 1 << nv;
-            if (dp[n_mask][nv] > dp[mask][cv] + adj[cv][nv])
-            {
-                dp[n_mask][nv] = dp[mask][cv] + adj[cv][nv];
-                pq.emplace(dp[n_mask][nv], n_mask, nv);
-            }
-        }
-    }
-
-    return ret;
-}
-
 int main(int argc, char const *argv[])
 {
     // #include <bits/stdc++.h>
@@ -75,7 +26,49 @@ int main(int argc, char const *argv[])
         for (int v = 1; v <= N; v++)
             cin >> adj[u][v];
 
-    int ans = dijkstra(1);
+    fill(&dp[0][0], &dp[0][0] + (1 << (NMX + 1)) * (NMX + 1), INF);
+
+    // 시작점을 1로
+    int sv = 1;
+    dp[1 << sv][sv] = 0;
+
+    int f_mask = (1 << (N + 1)) - 2;
+
+    for (int mask = 0; mask <= f_mask; mask++)
+    {
+        if (!(mask >> sv & 1))
+            continue;
+
+        for (int cv = 1; cv <= N; cv++)
+        {
+            if (!(mask >> cv & 1))
+                continue;
+
+            for (int nv = 1; nv <= N; nv++)
+            {
+                if (mask >> nv & 1)
+                    continue;
+
+                if (!adj[cv][nv])
+                    continue;
+
+                int n_mask = mask | 1 << nv;
+                dp[n_mask][nv] = min(dp[n_mask][nv], dp[mask][cv] + adj[cv][nv]);
+            }
+        }
+    }
+
+    int ans = INF;
+    for (int u = 1; u <= N; u++)
+    {
+        if (dp[f_mask][u] == INF)
+            continue;
+
+        if (!adj[u][sv])
+            continue;
+
+        ans = min(ans, dp[f_mask][u] + adj[u][sv]);
+    }
 
     cout << ans;
 

cee0e9d134f51433cfd6692e7f2adb66d8616247
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2026-01-02 18:23:27 +0900

    [Gold II] Title: 커플 만들기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/1727. 커플 만들기/커플 만들기.cc
+++ 백준/Gold/1727. 커플 만들기/커플 만들기.cc
@@ -16,64 +16,43 @@ int main(int argc, char const *argv[])
     int N, M;
     cin >> N >> M;
 
-    vector<vector<int>> arr(2);
-    arr[0].resize(N);
-    arr[1].resize(M);
-
+    vector<int> arr1(N);
     for (int i = 0; i < N; i++)
-        cin >> arr[0][i];
-
+        cin >> arr1[i];
+    vector<int> arr2(M);
     for (int i = 0; i < M; i++)
-        cin >> arr[1][i];
+        cin >> arr2[i];
 
-    sort(arr[0].begin(), arr[0].end());
-    sort(arr[1].begin(), arr[1].end());
-
-    const auto &arr_b = arr[N <= M];
-    const auto &arr_l = arr[N > M];
-    int B = arr_b.size();
-    int L = arr_l.size();
-
-    // dp[i] : arr_l이랑 arr_b[0, i] 까지 매칭시킬때 성격 차이 합이 최소가 되는 조합
-    vector<int> dp[1000];
-    dp[L - 1].resize(L);
-    iota(dp[L - 1].begin(), dp[L - 1].end(), 0);
-
-    for (int i = L; i < B; i++)
+    // 큰 쪽이 M이 되도록
+    if (N > M)
     {
-        dp[i] = dp[i - 1];
-        vector<int> diff_acc(L + 1);
-        for (int j = L - 1; j >= 0; j--)
-        {
-            int prv = dp[i][j];
-            int diff = abs(arr_b[prv + 1] - arr_l[j]) - abs(arr_b[prv] - arr_l[j]);
-
-            if (diff <= 0 && diff_acc[j + 1] + diff <= 0)
-            {
-                dp[i][j]++;
-                diff_acc[j] = 0;
-            }
-            else
-            {
-                diff_acc[j] = diff_acc[j + 1] + diff;
-            }
-        }
-
-        for (int j = 0; j < L - 1; j++)
-        {
-            if (dp[i][j] == dp[i][j + 1])
-                dp[i][j + 1]++;
-        }
+        swap(N, M);
+        swap(arr1, arr2);
     }
 
-    int ans = 0;
-    for (int j = 0; j < L; j++)
+    sort(arr1.begin(), arr1.end());
+    sort(arr2.begin(), arr2.end());
+
+    // dp[i][j] : arr1[0...i), arr2[0,,,j)를 매칭하는 최소 비용
+    /*
+        j를 늘려가면서 j - 1을 매칭하는 경우와 매칭하지 않는 경우를 비교
+        dp[i][j] = min(dp[i][j - 1], dp[i - 1][j - 1] + abs(arr1[i - 1] - arr2[j - 1]))
+    */
+
+    vector<int> prv(M + 1);
+    vector<int> cur(M + 1);
+
+    for (int i = 1; i <= N; i++)
     {
-        int bi = dp[B - 1][j];
-        ans += abs(arr_b[bi] - arr_l[j]);
+        cur[0] = INF;
+        for (int j = 1; j <= M; j++)
+        {
+            cur[j] = min(cur[j - 1], prv[j - 1] + abs(arr1[i - 1] - arr2[j - 1]));
+        }
+        swap(prv, cur);
     }
 
-    cout << ans;
+    cout << prv[M] << '\n';
 
     // inputFileStream.close();
     return 0;

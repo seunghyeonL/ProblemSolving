d0c08bede63a8c822107ff67c0a4d1819377ee59
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2026-01-02 21:09:14 +0900

    [Diamond V] Title: 라면 사기 (Large), Time: 104 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Diamond/18186. 라면 사기 （Large）/라면 사기 （Large）.cc
+++ 백준/Diamond/18186. 라면 사기 （Large）/라면 사기 （Large）.cc
@@ -30,38 +30,30 @@ int main(int argc, char const *argv[])
     // B > C
     ll ans = 0;
 
-    // for (ll n : arr)
-    //     cout << n << ' ';
-    // cout << '\n';
-
     for (int i = 0; i < N; i++)
     {
-        // cout << "----" << i << "------" << '\n';
         while (arr[i] > 0)
         {
-            if (i + 2 < N && arr[i] > 0 && arr[i] <= arr[i + 1] && arr[i + 1] <= arr[i + 2])
+            if (i + 2 < N && arr[i] <= arr[i + 1] && arr[i + 1] <= arr[i + 2])
             {
                 ll n = arr[i];
                 ans += n * (B + 2 * C);
                 arr[i] -= n;
                 arr[i + 1] -= n;
                 arr[i + 2] -= n;
-                // cout << 3 << ' ' << ans << '\n';
             }
-            else if (i + 1 < N && arr[i] > 0 && arr[i] <= arr[i + 1])
+            else if (i + 1 < N && arr[i] <= arr[i + 1])
             {
                 ll n = (i + 2 == N ? arr[i] : min(arr[i], arr[i + 1] - arr[i + 2]));
                 ans += n * (B + C);
                 arr[i] -= n;
                 arr[i + 1] -= n;
-                // cout << 2 << ' ' << ans << '\n';
             }
             else
             {
                 ll n = (i + 1 == N ? arr[i] : min(arr[i], arr[i] - arr[i + 1]));
                 ans += n * B;
                 arr[i] -= n;
-                // cout << 1 << ' ' << ans << '\n';
             }
         }
     }

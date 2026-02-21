d6b144b2cc18aaa3f75b56568b43e2345fb5e316
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2026-02-21 13:04:46 +0900

    [Gold IV] Title: 케이크 자르기, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17179. 케이크 자르기/케이크 자르기.cc
+++ 백준/Gold/17179. 케이크 자르기/케이크 자르기.cc
@@ -1,33 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MMX = 1000;
-const int LMX = 4000000;
 int N, M, L;
-int cn;
-vector<int> cut_pos;
-vector<int> cut_len;
+vector<int> D;
 
-// 모든 조각이 mn_len이상이 되도록 자를때 필요한 횟수가 cn 이상인가?
-bool check(int mn_len)
+// q번 자를때 모든 조각을 mn_len 이상으로 만들 수 있는가?
+// mn_len <= ans -> true
+// mn_len > ans -> false
+bool check(int mn_len, int q)
 {
-    int len = 0;
-    int pieces = 0; // 잘린 개수
-    for (int el : cut_len)
+    int pn = 0;
+    int cl = 0;
+
+    for (int i = 0; i <= M; i++)
     {
-        if (len + el >= mn_len)
+        cl += D[i];
+
+        if (cl >= mn_len)
         {
-            len = 0;
-            pieces++;
-        }
-        else
-        {
-            len += el;
+            pn++;
+            cl = 0;
         }
     }
 
-    // 더 많이 잘라서 만들 수 있으면 좀 덜잘라서 하면 정확히 처리 가능
-    if (pieces - 1 >= cn)
+    if (pn >= q + 1)
         return true;
     else
         return false;
@@ -44,38 +40,31 @@ int main(int argc, char const *argv[])
 
     // ifstream inputFileStream("input.txt");
 
-    /*
-        잘랐을때 제일 작은 길이가 최대가 되도록
-
-        모든 조각이 mn_len보다 커지게 할 수 있는가?
-    */
-
     cin >> N >> M >> L;
+    D.resize(M + 1);
 
-    cut_pos.push_back(0);
+    int prv = 0, cur = 0;
     for (int i = 0; i < M; i++)
     {
-        int pos;
-        cin >> pos;
-        cut_pos.push_back(pos);
+        cin >> cur;
+        D[i] = cur - prv;
+        prv = cur;
     }
-    cut_pos.push_back(L);
 
-    for (int i = 1; i <= M + 1; i++)
-    {
-        cut_len.push_back(cut_pos[i] - cut_pos[i - 1]);
-    }
+    D[M] = L - prv;
 
     for (int i = 0; i < N; i++)
     {
-        cin >> cn;
+        int q;
+        cin >> q;
 
-        int l = 1, r = LMX;
-
+        int l = 1;
+        int r = L;
         while (l <= r)
         {
             int m = (l + r) / 2;
-            if (check(m))
+
+            if (check(m, q))
                 l = m + 1;
             else
                 r = m - 1;
@@ -83,7 +72,6 @@ int main(int argc, char const *argv[])
 
         cout << r << '\n';
     }
-
     // inputFileStream.close();
     return 0;
 }

786adec68c440785540460d8313687580f7a9d24
seunghyeonL <91924090+seunghyeonL@users.noreply.github.com>
2026-04-19 19:51:15 +0900

    [level 4] Title: 징검다리, Time: 6.94 ms, Memory: 5.23 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.cpp
+++ 프로그래머스/4/43236. 징검다리/징검다리.cpp
@@ -1,65 +1,57 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-// 구간을 need개로 합칠때 최소구간 길이의 최댓값
-// 최소 길이가 특정 값 보다 작으면 항상 가능
-// 최소 길이가 특정 값 보다 크면 항상 불가능
-// -> 이분탐색
+// 모든 구간 거리를 mn_d 보다 크게 할때 구간 개수가 need개 이상인가?
+// (n 이상일 경우 구간 몇개를 합쳐서 n개로 만들기 가능)
+// mn_d =< ans -> true
+// mn_d > ans -> false
 
-int need;
-int N; // 구간 개수
-vector<int> D;
-
-// mn_len보다 큰 최소 길이로 구간을 만들어 나갈때
-// 개수가 need개 이상인가 (need개보다 많을 경우 합치면 더 길어지니 need개로 만들 수 있음)
-bool check(int mn_len)
+bool check(int mn_d, const vector<int>& dists, const int need)
 {
-    int pn = 0;
-    int cl = 0;
+    const int sz = dists.size();
     
-    for (int i = 0 ; i < N ; i++)
+    int pn = 0;
+    int cur = 0;
+    
+    for (int d : dists)
     {
-        cl += D[i];
-        
-        if (cl >= mn_len)
+        cur += d;
+        if (cur >= mn_d)
         {
             pn++;
-            cl = 0;
+            cur = 0;
         }
     }
     
-    if (pn >= need)
-        return true;
-    else 
-        return false;
+    return pn >= need;
 }
 
-int solution(int distance, vector<int> rocks, int n) 
-{
-    N = rocks.size() + 1;
-    need = N - n;
-    
-    rocks.push_back(0);
-    rocks.push_back(distance);
+
+int solution(int distance, vector<int> rocks, int n) {
+    const int N = rocks.size() + 1;
+    const int need = N - n;
     
     sort(rocks.begin(), rocks.end());
     
-    for (int i = 0 ; i < N ; i++)
-        D.push_back(rocks[i + 1] - rocks[i]);
+    vector<int> dists;
+    dists.reserve(N);
     
-    int l = 1, r = 1e9;
+    dists.push_back(rocks[0]);
+    for (int i = 1 ; i < N - 1 ; i++)
+        dists.push_back(rocks[i] - rocks[i - 1]);
+    dists.push_back(distance - rocks.back());
+    
+    int l = 1, r = distance;
     while (l <= r)
     {
         int m = (l + r) / 2;
         
-        if (check(m))
+        if (check(m, dists, need))
             l = m + 1;
         else
             r = m - 1;
     }
-    
-    for (int i = 0 ; i < N ; i++)
-        
     
     return r;
 }
